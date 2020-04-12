d1d25a27fe697e253bb2675315163ded8f44bc84
epistemik <epistemik@gmail.com>
2020-04-12 13:42:07 -0400

    store and print average of the estimates

--- problems/pi_gcd.py
+++ problems/pi_gcd.py
@@ -9,11 +9,18 @@ __author__         = 'Mark Sattolo'
 __author_email__   = 'epistemik@gmail.com'
 __python_version__ = '3.6+'
 __created__ = '2020-04-10'
-__updated__ = '2020-04-10'
+__updated__ = '2020-04-11'
 
 # Inspiration:
 #     Matt Parker: Generating π from 1,000 random numbers
 #     https://www.youtube.com/watch?v=RZBhSi_PwHU
+
+
+def average(pi_input:list) -> float:
+    result = 0.0
+    for i in pi_input:
+        result += i
+    return result/len(pi_input)
 
 
 def pi_estimate(num_samples:int, num_range:int, num_runs:int=10):
@@ -21,7 +28,8 @@ def pi_estimate(num_samples:int, num_range:int, num_runs:int=10):
     import math
     import random
 
-    print(F"            pi = {math.pi:>11.11f}")
+    estimates = list()
+    print(F"number range = {num_range} & number of samples (pairs of random #s) per run = {num_samples}")
 
     for r in range(num_runs):
         coprime = 0
@@ -35,8 +43,12 @@ def pi_estimate(num_samples:int, num_range:int, num_runs:int=10):
         fraction_coprime = coprime/num_samples
 
         est = math.sqrt(6.0/fraction_coprime)
+        estimates.append(est)
+        print(F"# of coprime pairs = {coprime} & estimate of pi = {est:>11.11f}")
 
-        print(F"estimate of pi = {est:>11.11f}")
+    ave_est = average(estimates)
+    print(F"\t\t    pi = {math.pi:>11.11f}")
+    print(F"average estimate of pi = {ave_est:>11.11f}")
 
 
 if __name__ == '__main__':
