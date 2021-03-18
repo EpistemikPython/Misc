821022e56d08753988183d672a37f0e6a4088877
epistemik <epistemik@gmail.com>
2021-03-18 09:38:24 -0400

    add main(), use functions, improve printing, more comments, etc

--- problems/problem_tavs.py
+++ problems/problem_tavs.py
@@ -3,7 +3,8 @@
 #
 # problem_tavs.py
 #
-#  Copyright (c) 2021  Thomas Sattolo  <tsattolo@gmail.com>
+#  original code Copyright (c) 2021  Thomas Sattolo  <tsattolo@gmail.com>
+#  modifications Copyright (c) 2021  Mark Sattolo  <epistemik@gmail.com>
 
 # In 1969, Hans Freudenthal posed a puzzle that Martin Gardner would later call “The Impossible Problem”.
 # Below is a 2000 version due to Erich Friedman.
@@ -24,21 +25,56 @@
 #
 # What are the numbers?
 
-pairs = list(set((j,i) for i in range(1,10) for j in range(1,i+1)))
-print(sorted(pairs))
-print()
+def run_rounds(pairs:list, num_rounds:int):
+    # the rounds
+    for nr in range(num_rounds):
+        print(F"Round #{nr+1}.")
+        p = [a*b for a,b in pairs]
+        print(F"products = {repr(p)}")
+        save_p = list()
+        # find unique products
+        for i,pz in enumerate(pairs):
+            if p.count(p[i]) == 1:
+                save_p.append(i)
+        print(F"product indices to remove = {repr(save_p)}")
+        # print(pairs[0])
+        # get rid of unique products
+        for ix in reversed(save_p):
+            # print(ix)
+            del pairs[ix]
+        print(F"now pairs = {repr(pairs)}")
 
-for _ in range(4):
-    p = [a*b for a,b in pairs]
-    print(p)
-    for i,(a,b) in enumerate(pairs):
-        if p.count(p[i]) == 1:
-            del pairs[i]
-    print(pairs)
+        s = [a+b for a,b in pairs]
+        print(F"sums = {repr(s)}")
+        save_s = list()
+        # find unique sums
+        for i,sz in enumerate(pairs):
+            if s.count(s[i]) == 1:
+                save_s.append(i)
+        print(F"sum indices to remove = {repr(save_s)}")
+        # get rid of unique sums
+        for iy in reversed(save_s):
+            del pairs[iy]
+        print(F"now pairs = {repr(pairs)}\n")
 
-    s = [a+b for a,b in pairs]
-    for i,(a,b) in enumerate(pairs):
-        if s.count(s[i]) == 1:
-            del pairs[i]
-    print(pairs)
-    print()
+
+def main_two_digits_problem(num_rounds:int):
+    pairs = set( (j, i) for i in range(1, 10) for j in range(1, i + 1) )
+    pairs = sorted( list(pairs) )
+    print(F"initial pairs = {repr(pairs)}")
+    print(F"number of pairs = {len(pairs)}\n")
+
+    run_rounds(pairs, num_rounds)
+
+    # unique product left gives the numbers
+    print("Finished all rounds.")
+    p1 = [a * b for a, b in pairs]
+    print(F"remaining products = {repr(p1)}")
+    for i,r in enumerate(pairs):
+        if p1.count(p1[i]) == 1:
+            print(F"unique remaining product = {p1[i]}\nnumbers are {repr(pairs[i])}")
+
+
+if __name__ == '__main__':
+    main_two_digits_problem(4)
+    exit()
