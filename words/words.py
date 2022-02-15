4e15fc5fb88f7160b981591c308e5d77d1aafd5e
epistemik <epistemik@gmail.com>
2022-02-15 07:58:08 -0500

    initial commit

---
+++ words/words.py
@@ -0,0 +1,13 @@
+##############################################################################################################################
+# coding=utf-8
+#
+# words.py -- manipulate a words dict
+#
+# Copyright (c) 2022 Mark Sattolo <epistemik@gmail.com>
+
+__author__         = "Mark Sattolo"
+__author_email__   = "epistemik@gmail.com"
+__python_version__ = "3.6+"
+__created__ = "2022-02-15"
+__updated__ = "2022-02-15"
+
