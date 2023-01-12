fbb18526a0d4aea5a6fa48daaea9e283e9aeb6c8
epistemik <epistemik@gmail.com>
2023-01-12 14:37:31 -0500

    add a main function and make saving to json a command line option, with default 'No'

--- words/words.py
+++ words/words.py
@@ -1,7 +1,7 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# words.py -- manipulate a words dict
+# words.py -- get the length of each item in a words dict
 #
 # Copyright (c) 2022 Mark Sattolo <epistemik@gmail.com>
 
@@ -9,26 +9,41 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2022-02-15"
-__updated__ = "2022-02-15"
+__updated__ = "2023-01-12"
 
-from sys import path
+from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
 from mhsUtils import save_to_json
 from words_2019 import eng_words
 
-newdict = {}
-ie = 0
-for item in eng_words:
-    newdict[item] = len(item)
-    ie += 1
+def main_words(save_option:str):
+    newdict = {}
+    ie = 0
+    for item in eng_words:
+        newdict[item] = len(item)
+        ie += 1
 
-print(f"eng_words count = {ie}\n")
+    print(f"words count = {ie}\n")
 
-ni = 0
-for item in newdict:
-    print(f"newdict[{item}] = {newdict[item]}")
-    ni += 1
-    if ni > 30:
-        break
+    ni = 0
+    np = 0
+    for item in newdict:
+        ni += 1
+        if ni % 1313 == 0:
+            # some miscellaneous entries
+            print(f"newdict[{item}] = {newdict[item]}")
+            np += 1
+        if np > 66:
+            break
 
-save_to_json("len_words", newdict)
+    save_option = save_option.upper()
+    if save_option == 'Y' or save_option == 'YES':
+        save_to_json("len_words", newdict)
+
+
+if __name__ == '__main__':
+    save_opt = 'No'
+    if len(argv) > 1 and argv[1].isalpha():
+        save_opt = argv[1]
+    main_words(save_opt)
+    exit()
