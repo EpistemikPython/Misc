c1aa29882c33556a1c42e38923ce237ab46c6149
epistemik <epistemik@gmail.com>
2023-10-12 16:24:29 -0400

    updates and improvements

--- words/find_words.py
+++ words/find_words.py
@@ -1,8 +1,9 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# find_words.py -- process a scrabble words dict to record the score of each word
+# find_words.py -- process a list of words to find a group of words of the same length with each having unique letters
 # adapted from: https://github.com/sh1boot/fivewords.git
+# see: https://www.youtube.com/watch?v=c33AZBnRHks
 #
 # Copyright (c) 2023 Mark Sattolo <epistemik@gmail.com>
 
@@ -10,7 +11,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-10"
-__updated__ = "2023-10-11"
+__updated__ = "2023-10-12"
 
 from sys import argv
 import time
@@ -24,6 +25,12 @@ firstletter = [ {} for f in range(26) ]
 wordnames = {}
 output = {}
 count = 0
+default_wordsize = 5
+default_numwords = 5
+five_words_initializer = (0, 0, 0, 0, 0)
+four_words_initializer = (0, 0, 0, 0)
+three_words_initializer = (0, 0, 0)
+ordered_letters = 'aesiorunltycdhmpgkbwfvzjxq'
 
 # TODO: find the proper balance between these two implementations
 #
@@ -43,7 +50,7 @@ def decompress(dpack):
     return 1<<dpack
 
 def letter_to_bit(clb):
-    return 'aesiorunltycdhmpgkbwfvzjxq'.find(clb)
+    return ordered_letters.find(clb)
     # return ord(c) - ord('a')
 
 def store(progress):
@@ -58,7 +65,7 @@ def store(progress):
         output[count] = group
         count += 1
 
-def solve(alphabet, num_wds, grace = 1, progress = array.array('L', (0, 0, 0, 0, 0)), depth=0):
+def solve(alphabet, num_wds, grace, progress, depth=0):
     depth_limit = num_wds - 1
     for drop in range(grace + 1):
         first = alphabet.bit_length() - 1
@@ -74,7 +81,8 @@ def solve(alphabet, num_wds, grace = 1, progress = array.array('L', (0, 0, 0, 0,
                             solve( alphabet & ~mask, num_wds, grace - drop, progress, depth + 1 )
         alphabet ^= 1<<first
 
-def main_find(save_option, word_sz=5, num_wds=5):
+#TODO: Add an 'exclude' parameter to exclude certain letters from consideration
+def main_find(save_option, word_sz, num_wds):
     print(f"find {num_wds} unique words each with {word_sz} letters.")
     print(f"save option = '{save_option}'\n")
     start = time.perf_counter()
@@ -98,28 +106,39 @@ def main_find(save_option, word_sz=5, num_wds=5):
                 firstletter[first].setdefault( pack, set() ).add(mask)
 
     alphabet = (1<<26) - 1
-    solve(alphabet, num_wds)
+    initializer = five_words_initializer
+    if num_wds == 4:
+        initializer = four_words_initializer
+    elif num_wds == 3:
+        initializer = three_words_initializer
+    starter = array.array('L', initializer)
+    solve(alphabet, num_wds, 1, starter)
     # print(output)
+    nx = 0
     for index in output.keys():
         print(output[index])
+        nx += 1
+        if nx > 25:
+            break
     print(f"total groups = {count}")
     print(f"\nelapsed time = {time.perf_counter() - start}")
 
     save_option = save_option.upper()
     if save_option == 'Y' or save_option == 'YES':
         save_to_json(f"{word_sz}x{num_wds}_words", output)
+    print(f"\nelapsed time = {time.perf_counter() - start}")
 
 
 if __name__ == '__main__':
     save_opt = 'No'
-    word_size = 5
-    num_words = 5
+    word_size = default_wordsize
+    num_words = default_numwords
     print(f"argv = {argv}")
     if len(argv) > 1 and argv[1].isalpha():
         save_opt = argv[1]
-    if len(argv) > 2 and argv[2].isalpha():
+    if len(argv) > 2:
         word_size = int(argv[2])
-    if len(argv) > 3 and argv[3].isalpha():
+    if len(argv) > 3:
         num_words = int(argv[3])
     main_find(save_opt, word_size, num_words)
     exit()
