08bd331f790949a6737b29d2b4d5a3203b2a4f2f
epistemik <epistemik@gmail.com>
2023-10-16 08:10:14 -0400

    check the number of words requested; add parameter type hints; additional info printing; various optimizations

--- words/find_words.py
+++ words/find_words.py
@@ -1,7 +1,7 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# find_words.py -- process a list of words to find a group of words of the same length with each having unique letters
+# find_words.py -- process a list of words to find groups of words of the same length with each having unique letters
 # adapted from: https://github.com/sh1boot/fivewords.git
 # see: https://www.youtube.com/watch?v=c33AZBnRHks
 #
@@ -11,7 +11,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-10"
-__updated__ = "2023-10-15"
+__updated__ = "2023-10-16"
 
 import array
 import time
@@ -19,7 +19,7 @@ from sys import argv
 from mhsUtils import save_to_json
 from scrabble_words_2019 import scrabble
 
-# highest to lowest letter frequencies in scrabble 5-letter words
+# highest to lowest letter frequencies in Scrabble 5-letter words
 ordered_letters = 'seaoriltnudycpmhgbkfwvzjxq'
 default_num_letts = len(ordered_letters)
 min_num_letts = 12
@@ -27,25 +27,23 @@ wordnames = {}
 output = {}
 count = 0
 default_wordsize = 5
-five_words_initializer = (0, 0, 0, 0, 0)
-default_numwords = len(five_words_initializer)
-four_words_initializer = (0, 0, 0, 0)
-three_words_initializer = (0, 0, 0)
-two_words_initializer = (0, 0)
+default_initializer = (0, 0, 0, 0, 0)
+default_numwords = len(default_initializer)
+min_numwords = 2
 
-def compress(cword):
+def compress(cword:int):
     return (cword ^ (cword - 1)).bit_length() - 1
 
-def decompress(dpack):
+def decompress(dpack:int):
     return 1<<dpack
 
-def store(group):
+def store(group:array):
     global count
-    group = sorted( next( iter(wordnames[word]) ) for word in group )
+    group = sorted( next(iter(wordnames[word])) for word in group )
     output[count] = group
     count += 1
 
-def solve(alphabet, num_wrds, grace, progress, depth=0):
+def solve(alphabet:int, num_wrds:int, grace:int, progress:array, depth:int=0):
     depth_limit = num_wrds - 1
     for drop in range(grace + 1):
         first = alphabet.bit_length() - 1
@@ -61,11 +59,10 @@ def solve(alphabet, num_wrds, grace, progress, depth=0):
                             solve(alphabet & ~mask, num_wrds, grace - drop, progress, depth + 1)
         alphabet ^= 1<<first
 
-#TODO: Add a parameter to set the number of letters used
-def main_find(save_option, word_sz, num_wds):
-    print(f"find {num_wds} 'uniquely lettered' words each with {word_sz} letters.")
+def main_find(save_option:str, word_sz:int, num_wds:int):
     print(f"letters to use are: '{ordered_letters}'")
-    print(f"save option = '{save_option}'\n")
+    print(f"find {num_wds} 'uniquely lettered' words each with {word_sz} letters.")
+    print(f"save option = '{save_option}'")
     start = time.perf_counter()
 
     for word in scrabble.keys():
@@ -74,7 +71,7 @@ def main_find(save_option, word_sz, num_wds):
             word = word.lower()
             for lett in word:
                 i = ordered_letters.find(lett)
-                if not(0 <= i < lett_range):
+                if not(0 <= i < num_letts):
                     break
                 b = 1<<i
                 if mask & b:
@@ -86,18 +83,19 @@ def main_find(save_option, word_sz, num_wds):
                 wordnames.setdefault( mask, set() ).add(word)
                 firstletter[first].setdefault( pack, set() ).add(mask)
 
-    alphabet = (1<<lett_range) - 1
-    initializer = five_words_initializer
+    alphabet = (1<<num_letts) - 1
+    initializer = default_initializer
     if num_wds == 4:
-        initializer = four_words_initializer
+        initializer = (0, 0, 0, 0)
     elif num_wds == 3:
-        initializer = three_words_initializer
-    elif num_wds == 2:
-        initializer = two_words_initializer
-    grace = lett_range + 1 - (word_sz * num_wds)
+        initializer = (0, 0, 0)
+    elif num_wds == min_numwords:
+        initializer = (0, 0)
+    grace = num_letts - (word_sz * num_wds)
     if grace < 0:
         print(f"BAD grace = {grace}!")
         exit(grace)
+    print(f"grace = {grace}\n")
     solve(alphabet, num_wds, grace, array.array('L', initializer))
 
     # sample some of the output
@@ -111,8 +109,8 @@ def main_find(save_option, word_sz, num_wds):
     print(f"\nelapsed time = {time.perf_counter() - start}")
 
     save_option = save_option.upper()
-    if save_option == 'Y' or save_option == 'YES':
-        save_to_json(f"{word_sz}x{num_wds}_words", output)
+    if save_option[0] == 'Y' and len(save_option) <= 4:
+        save_to_json(f"{word_sz}x{num_wds}f{num_letts}_words", output)
         print(f"\nelapsed time = {time.perf_counter() - start}")
 
 
@@ -127,19 +125,25 @@ if __name__ == '__main__':
         save_opt = argv[1]
     if len(argv) > 2:
         word_size = int(argv[2])
+        print(f"requested word size = {word_size}")
     if len(argv) > 3:
         num_words = int(argv[3])
+        print(f"number of words requested = {num_words}")
     if len(argv) > 4:
         num_letters_to_use = int(argv[4])
+        print(f"number of letters requested = {num_letters_to_use}")
 
+    # make sure all the parameters regarding size of words and number of words and letters are safe and sensible
+    if default_numwords < num_words < min_numwords:
+        num_words = default_numwords
     if min_num_letts <= num_letters_to_use < default_num_letts:
         ordered_letters = ordered_letters[:num_letters_to_use]
-    print(f"number of letters to use = '{num_letters_to_use}'")
     if word_size * num_words > num_letters_to_use:
         word_size = default_wordsize
         num_words = num_letters_to_use // default_wordsize
-    lett_range = len(ordered_letters)
-    firstletter = [{} for f in range(lett_range)]
+    num_letts = len(ordered_letters)
+    print(f"using {num_letts} letters")
+    firstletter = [{} for f in range(num_letts)]
 
     main_find(save_opt, word_size, num_words)
     exit()
