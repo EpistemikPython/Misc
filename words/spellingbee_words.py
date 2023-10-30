17a54fc452b6c522d48281bd09415bb71c1a28b6
epistemik <epistemik@gmail.com>
2023-10-30 12:48:55 -0400

    check for pangrams and display results; minor changes to regularize with other word scripts

--- words/spellingbee_words.py
+++ words/spellingbee_words.py
@@ -9,7 +9,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2023-10-29"
+__updated__ = "2023-10-30"
 
 import time
 import json
@@ -21,39 +21,51 @@ start = time.perf_counter()
 DEFAULT_OUTER_LETTERS = "CONLAY"
 GROUP_SIZE = len(DEFAULT_OUTER_LETTERS)
 DEFAULT_REQD_LETTER = "I"
-solutions = []
 MIN_WORD_SIZE = 4
 
 def main_sb():
+    """find all words from a list that fulfill the specified spelling bee requirements"""
+    solutions = []
     sct = 0
-    scp = json.load(open("scrabble-plus.json"))
-    for item in scp:
+    spj = json.load(open("scrabble-plus.json"))
+    for item in spj:
         if len(item) >= MIN_WORD_SIZE:
             for letter in item:
-                if not (letter in DEFAULT_OUTER_LETTERS + DEFAULT_REQD_LETTER):
+                if not (letter in group + required):
                     break
             else:
-                if DEFAULT_REQD_LETTER in item:
+                if required in item:
                     solutions.append(item)
                     sct += 1
 
     print(f"solution count = {sct}")
-    print(f"solutions = {solutions}")
-    # rev_freqs = {}
-    # print(f"\nsorted letter frequencies:")
-    # for val in sorted(rev_freqs, reverse = True):
-    #     print(f"{rev_freqs[val]}: {val}")
+    # check for pangrams
+    pgct = 0
+    print(f"solutions:")
+    for val in solutions:
+        pg = True
+        for lett in group:
+            if not lett in val:
+                pg = False
+                break
+        if pg:
+            print(f"\t{val} : Pangram!")
+            pgct += 1
+        else:
+            print(f"\t{val}")
+    if pgct == 0:
+        print(">> NO Pangrams!!")
 
-    global save_option
-    save_option = save_option.upper()
-    if save_option == 'Y' or save_option == 'YES':
-        save_to_json("letter_frequencies", solutions)
+    print(f"\nelapsed time = {time.perf_counter() - start}")
+    if save_option.upper()[0] == 'Y':
+        save_to_json(f"{required}-{group}_spellingbee_words", solutions)
 
 
 if __name__ == '__main__':
-    save_option = 'No'
+    save_option = "No"
     if len(argv) > 1 and argv[1].isalpha():
         save_option = argv[1]
+    print(f"save option = '{save_option}'")
 
     group = DEFAULT_OUTER_LETTERS
     required = DEFAULT_REQD_LETTER
@@ -61,12 +73,13 @@ if __name__ == '__main__':
         request = argv[2]
         if len(request) == 1 and request.isalpha():
             print(f"requested necessary letter = {request}")
-            required = request
+            required = request.upper()
     if len(argv) > 3:
         request = argv[3]
         if len(request) == GROUP_SIZE and request.isalpha():
+            # make sure all the letters are different?
             print(f"requested outer letters = {request}")
-            group = request
+            group = request.upper()
     print(f"required letter = {required}")
     print(f"outer letters = {group}")
 
