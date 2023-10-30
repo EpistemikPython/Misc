c6918cb7bafa2b42d2d92aec169fe3268f41f425
epistemik <epistemik@gmail.com>
2023-10-30 12:44:48 -0400

    regularize with other word scripts

--- words/choose_words.py
+++ words/choose_words.py
@@ -9,7 +9,7 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2023-10-29"
+__updated__ = "2023-10-30"
 
 import time
 from sys import path, argv
@@ -22,8 +22,8 @@ DEFAULT_LENGTH = 5
 MIN_LENGTH = 2
 MAX_LENGTH = 15
 
-def main_choose(save_option:str):
-    print(f"save option = '{save_option}'\n")
+def main_choose():
+    """process a list of words to find words of the specified lengths and save to a json file"""
     newlist = []
     ix = 0
     for item in scrabble.keys():
@@ -40,15 +40,15 @@ def main_choose(save_option:str):
         ni += 1
 
     print(f"\nelapsed time = {time.perf_counter() - start}")
-    save_option = save_option.upper()
-    if save_option == 'Y' or save_option == 'YES':
+    if save_option.upper()[0] == 'Y':
         save_to_json(f"{lower}-{upper}_letter_words", newlist)
 
 
 if __name__ == '__main__':
-    save_opt = 'No'
+    save_option = "No"
     if len(argv) > 1 and argv[1].isalpha():
-        save_opt = argv[1]
+        save_option = argv[1]
+    print(f"save option = '{save_option}'")
 
     lower = DEFAULT_LENGTH
     upper = DEFAULT_LENGTH
@@ -63,10 +63,10 @@ if __name__ == '__main__':
         if MIN_LENGTH < request <= MAX_LENGTH:
             upper = request
     if lower > upper:
-        lower = upper-1
+        lower = upper - 1
     print(f"lower word size = {lower}")
     print(f"upper word size = {upper}")
 
-    main_choose(save_opt)
+    main_choose()
     print(f"\nelapsed time = {time.perf_counter() - start}")
     exit()
