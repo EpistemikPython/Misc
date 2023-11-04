5a628fa0ba94f68820025e620d2799b2a0172468
epistemik <epistemik@gmail.com>
2023-11-04 16:19:40 -0400

    use mhsLogging

--- words/spellingbee_words.py
+++ words/spellingbee_words.py
@@ -9,13 +9,19 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2023-10-30"
+__updated__ = "2023-11-03"
 
 import time
 import json
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
-from mhsUtils import save_to_json
+from mhsUtils import save_to_json, get_base_filename
+from mhsLogging import MhsLogger
+
+log_control = MhsLogger(get_base_filename(__file__))
+lgr = log_control.get_logger()
+lgr.warning("START LOGGING")
+show = log_control.show
 
 start = time.perf_counter()
 WORD_FILE = "scrabble-plus.json"
@@ -39,10 +45,10 @@ def main_sb():
                     solutions.append(item)
                     sct += 1
 
-    print(f"solution count = {sct}")
+    show(f"solution count = {sct}")
     # check for pangrams and print the solutions
     pgct = 0
-    print(f"solutions:")
+    show(f"solutions:")
     for val in solutions:
         pg = True
         for lett in group:
@@ -50,42 +56,41 @@ def main_sb():
                 pg = False
                 break
         if pg:
-            print(f"\t{val} : Pangram!")
+            show(f"\t{val} : Pangram!")
             pgct += 1
         else:
-            print(f"\t{val}")
-    if pgct == 0:
-        print(">> NO Pangrams!!")
+            show(f"\t{val}")
+    show(f">> {pgct} Pangram{'' if pgct == 1 else 's'}!!")
 
-    print(f"\nelapsed time = {time.perf_counter() - start}")
+    show(f"\nelapsed time = {time.perf_counter() - start}")
     if save_option.upper()[0] == 'Y':
-        save_to_json(f"{required}-{group}_spellingbee_words", solutions)
+        save_to_json(f"{required}-{group}_spellbee_words", solutions)
 
 
 if __name__ == '__main__':
-    print(f"word file = '{WORD_FILE}'")
+    show(f"word file = '{WORD_FILE}'")
     save_option = "No"
     if len(argv) > 1 and argv[1].isalpha():
         save_option = argv[1]
-    print(f"save option = '{save_option}'")
+    show(f"save option = '{save_option}'")
 
     required = DEFAULT_REQD_LETTER
     if len(argv) > 2:
         request = argv[2]
         if len(request) == 1 and request.isalpha():
-            print(f"requested mandatory letter = {request}")
+            show(f"requested mandatory letter = {request}")
             required = request.upper()
-    print(f"required letter = {required}")
+    show(f"required letter = {required}")
     # make sure the required letter is not among the outer letters?
     group = DEFAULT_OUTER_LETTERS
     if len(argv) > 3:
         request = argv[3]
         if len(request) == GROUP_SIZE and request.isalpha():
             # make sure all the letters are different?
-            print(f"requested outer letters = {request}")
+            show(f"requested outer letters = {request}")
             group = request.upper()
-    print(f"outer letters = {group}")
+    show(f"outer letters = {group}")
 
     main_sb()
-    print(f"\nelapsed time = {time.perf_counter() - start}")
+    show(f"\nelapsed time = {time.perf_counter() - start}")
     exit()
