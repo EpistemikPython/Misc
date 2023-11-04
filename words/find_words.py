b80b964c3ed59c3c426b273be74f9615afc68b90
epistemik <epistemik@gmail.com>
2023-11-04 16:18:00 -0400

    improve var names; use word file; &

--- words/find_words.py
+++ words/find_words.py
@@ -11,15 +11,16 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-10"
-__updated__ = "2023-10-30"
+__updated__ = "2023-11-03"
 
 import array
+import json
 import time
 from sys import argv
 from mhsUtils import save_to_json
-from scrabble_words_2019 import scrabble
 
 start = time.perf_counter()
+WORD_FILE = "scrabble-plus.json"
 # highest to lowest letter frequencies in Scrabble '5-13 letter' words
 ORDERED_LETTERS = "ESIARNOTLCDUPMGHBYFKVWZXQJ"
 DEFAULT_NUMLETTERS = len(ORDERED_LETTERS)
@@ -35,62 +36,63 @@ count = 0
 wordnames = {}
 output = {}
 
-def compress(cword:int):
-    return (cword ^ (cword - 1)).bit_length() - 1
+def compress(cmask:int):
+    return (cmask^(cmask - 1)).bit_length() - 1
 
 def decompress(dpack:int):
     return 1<<dpack
 
 def store(group:array):
     global count
-    group = sorted( next(iter(wordnames[word])) for word in group )
+    group = sorted( next(iter(wordnames[wmask])) for wmask in group )
     output[count] = group
     count += 1
 
-def solve(alphabet:int, p_grace:int, progress:array, depth:int=0):
+def solve(highbit:int, p_extra:int, progress:array, depth:int=0):
     depth_limit = num_words - 1
-    for drop in range(p_grace + 1):
-        first = alphabet.bit_length() - 1
-        for pack in firstletter[first]:
+    for dx in range(p_extra + 1):
+        least = highbit.bit_length() - 1
+        for pack in lc_letter[least]:
             required = decompress(pack)
-            if( alphabet & required ) == required:
-                for mask in firstletter[first][pack]:
-                    if( mask & alphabet ) == mask:
+            if(highbit & required) == required:
+                for mask in lc_letter[least][pack]:
+                    if(mask & highbit) == mask:
                         progress[depth] = mask
                         if depth >= depth_limit:
                             store(progress)
                         else:
-                            solve(alphabet & ~mask, p_grace - drop, progress, depth + 1)
-        alphabet ^= 1<<first
+                            solve(highbit & ~mask, p_extra - dx, progress, depth + 1)
+        highbit ^= 1<<least
 
 def main_find():
     """process a list of words to find groups of words of the same length with each having unique letters"""
     ct = 0
-    first = 0
-    for word in scrabble.keys():
+    least = 0
+    wf = json.load( open(WORD_FILE) )
+    for word in wf:
         if len(word) == word_size:
             ct += 1
             mask = 0
             for lett in word:
-                i = ordered_letters.find(lett)
-                if not(0 <= i < num_letters):
+                lx = ordered_letters.find(lett)
+                if 0 > lx >= num_letters:
                     break
-                b = 1<<i
-                if mask & b:
+                bx = 1<<lx
+                if mask & bx:
                     break
-                mask |= b
+                mask |= bx
             else:
-                first = mask.bit_length() - 1
+                least = mask.bit_length() - 1
                 pack = compress(mask)
                 wordnames.setdefault( mask, set() ).add(word)
-                firstletter[first].setdefault( pack, set() ).add(mask)
+                lc_letter[least].setdefault(pack, set()).add(mask)
 
     # DEBUG
     print(f"found {ct} words.")
     print(f"len wordnames = {len(wordnames)}")
-    print(f"first = {first}")
-    print(f"len firstletter[first] = {len(firstletter[first])}")
-    print(f"firstletter[first] = {firstletter[first]}")
+    print(f"last 'least' = {least}")
+    print(f"len lc_letter[least] = {len(lc_letter[least])}")
+    print(f"lc_letter[least] = {lc_letter[least]}")
 
     initializer = DEFAULT_INITIALIZER
     if num_words == 4:
@@ -99,9 +101,10 @@ def main_find():
         initializer = (0, 0, 0)
     elif num_words == MIN_NUMWORDS:
         initializer = (0, 0)
-    solve((1<<num_letters) - 1, grace, array.array('L', initializer))
+    solve((1<<num_letters) - 1, extra, array.array('L', initializer))
 
     # sample some of the output
+    print(f"\nsolutions:")
     nx = 0
     for index in output.keys():
         print(output[index])
@@ -148,12 +151,9 @@ if __name__ == '__main__':
         word_size = DEFAULT_WORDSIZE
         num_words = num_letters // DEFAULT_WORDSIZE
     print(f"find {num_words} 'uniquely lettered' words each with {word_size} letters.")
-    grace = num_letters - (word_size * num_words)
-    print(f"grace = {grace}\n")
-
-    firstletter = [{} for f in range(num_letters)]
-    # DEBUG
-    print(firstletter)
+    extra = num_letters - (word_size * num_words)
+    print(f"num extra letters = {extra}\n")
+    lc_letter = [{} for f in range(num_letters)]
 
     main_find()
     print(f"\nelapsed time = {time.perf_counter() - start}")
