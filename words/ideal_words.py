297a037dd703b0e353d5907fdb5bf24cee8d3fd2
epistemik <epistemik@gmail.com>
2023-11-24 16:51:57 -0500

    improvements

--- words/ideal_words.py
+++ words/ideal_words.py
@@ -9,7 +9,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-11-22"
-__updated__ = "2023-11-23"
+__updated__ = "2023-11-24"
 
 import time
 import json
@@ -19,11 +19,6 @@ path.append("/home/marksa/git/Python/utils")
 from mhsUtils import save_to_json, get_base_filename
 from mhsLogging import MhsLogger
 
-log_control = MhsLogger(get_base_filename(__file__))
-lgr = log_control.get_logger()
-lgr.warning("START LOGGING")
-show = log_control.show
-
 start = time.perf_counter()
 WORD_FILE = "scrabble-plus.json"
 DEFAULT_EXTRA_LETTERS = "NDWPGHVJKXQZ"
@@ -31,66 +26,75 @@ MAX_EXTRA_LETTERS = 20
 DEFAULT_REQUIRED_LETTERS = "AEO"
 MAX_REQUIRED_LETTERS = 5
 DEFAULT_WORD_SIZE = 7
+MIN_WORD_SIZE = 5
+MAX_WORD_SIZE = 15
 
-def run_ideal(save_option, required, group):
-    """from a word list find all the words that contain only the specified letters"""
+def run_ideal(save_option:bool, word_size:int, required:str, group:str):
+    """from a word list find all the words of the specified length that contain only the specified letters"""
 
     solutions = []
     sct = 0
     wdf = json.load( open(WORD_FILE) )
+    show(f"loaded file '{WORD_FILE}'")
     for item in wdf:
-        if len(item) == DEFAULT_WORD_SIZE:
-            # good = True
+        if len(item) == word_size:
             for letter in item:
                 if letter not in group + required:
                     break
             else:
                 for lt in required:
                     if lt not in item:
-                        # good = False
                         break
                 else:
                     solutions.append(item)
                     sct += 1
 
     show(f"solution count = {sct}")
-    # print the solutions
-    show(f"solutions:")
+    # display the solutions
+    show("solutions:")
     for val in solutions:
         show(f"\t{val}")
 
-    show(f"\nelapsed time = {time.perf_counter() - start}")
-    if save_option.upper()[0] == 'Y':
+    show(f"\nsolve & display elapsed time = {time.perf_counter() - start}")
+    if save_option:
         save_to_json(f"{required}-{group}_ideal_words", solutions)
 
 
 def process_args():
-    arg_parser = ArgumentParser(description="get the save, required letters and possible letters options", prog="ideal_words.py")
+    arg_parser = ArgumentParser(description="get the save-to-file, word size, required letters and possible letters options", prog="ideal_words.py")
     # optional arguments
-    arg_parser.add_argument('-s', '--save',  action="store_true", default=False, help="Write the results to a JSON file")
-    arg_parser.add_argument('-r', '--required', type=str, default=DEFAULT_REQUIRED_LETTERS, help="letters MUST be in the words")
-    arg_parser.add_argument('-o', '--other', type=str, default=DEFAULT_EXTRA_LETTERS, help="possible letters in the words")
+    arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
+    arg_parser.add_argument('-n', '--numletters', type=int, default=DEFAULT_WORD_SIZE, help="number of letters in each found word")
+    arg_parser.add_argument('-r', '--required', type=str, default=DEFAULT_REQUIRED_LETTERS, help="each of these letters MUST be in each word")
+    arg_parser.add_argument('-o', '--other', type=str, default=DEFAULT_EXTRA_LETTERS, help="other possible letters in the words")
     return arg_parser
 
 
 def main_ideal(argl:list):
     args = process_args().parse_args(argl)
 
-    save_option = "Yes" if args.save else "No"
-    show(f"save option = '{save_option}'")
+    show(f"save option = '{args.save}'")
 
-    required = args.required.upper()
+    word_size = args.numletters if MIN_WORD_SIZE <= args.numletters <= MAX_WORD_SIZE else DEFAULT_WORD_SIZE
+    show(f"word size = {word_size}")
+
+    required = args.required.upper() if len(args.required) <= MAX_REQUIRED_LETTERS else DEFAULT_REQUIRED_LETTERS
     # make sure the required letters are not among the other letters?
     show(f"required letters = {required}")
 
-    group = args.other.upper()
+    others = args.other.upper() if len(args.other) <= MAX_EXTRA_LETTERS else DEFAULT_EXTRA_LETTERS
     # make sure all the letters are different?
-    show(f"other letters = {group}")
+    show(f"other letters = {others}")
 
-    run_ideal(save_option, required, group)
+    run_ideal(args.save, word_size, required, others)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    log_control = MhsLogger(get_base_filename(__file__))
+    lgr = log_control.get_logger()
+    lgr.warning("START LOGGING")
+    show = log_control.show
+
     main_ideal(argv[1:])
-    show(f"\nelapsed time = {time.perf_counter() - start}")
+    show(f"\nfinal elapsed time = {time.perf_counter() - start}")
     exit()
