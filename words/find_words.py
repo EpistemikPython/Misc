bf104715171895bd6b32527082ead291cd63147a
epistemik <epistemik@gmail.com>
2023-12-15 16:43:24 -0500

    convert to using ArgumentParser with attendant changes and also improvements; move logging init to 'main'; &

--- words/find_words.py
+++ words/find_words.py
@@ -11,30 +11,28 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-10"
-__updated__ = "2023-11-15"
+__updated__ = "2023-12-15"
 
 import array
 import json
 import time
-from sys import argv
-from mhsUtils import save_to_json
+from argparse import ArgumentParser
+from sys import path, argv
+path.append("/home/marksa/git/Python/utils")
+from mhsUtils import save_to_json, get_base_filename
+from mhsLogging import MhsLogger
 
 start = time.perf_counter()
 WORD_FILE = "scrabble-plus.json"
 # highest to lowest letter frequencies in Scrabble '5-13 letter' words
 ORDERED_LETTERS = "ESIARNOTLCDUPMGHBYFKVWZXQJ"
-DEFAULT_NUMLETTERS = len(ORDERED_LETTERS)
+MAX_NUMLETTERS = len(ORDERED_LETTERS)
 MIN_NUM_LETTERS = 12
 MAX_WORDSIZE = 15
-DEFAULT_WORDSIZE = 5
+MIN_WORDSIZE = 5
 DEFAULT_INITIALIZER = (0, 0, 0, 0, 0)
 MAX_NUMWORDS = len(DEFAULT_INITIALIZER)
 MIN_NUMWORDS = 2
-
-# global vars
-count = 0
-wordnames = {}
-output = {}
 
 def compress(cmask:int):
     return (cmask ^ (cmask - 1)).bit_length() - 1
@@ -44,7 +42,7 @@ def decompress(dpack:int):
 
 def store(group:array):
     global count
-    group = sorted( next(iter(wordnames[wmask])) for wmask in group )
+    group = sorted( next(iter(word_names[wmask])) for wmask in group )
     output[count] = group
     count += 1
 
@@ -52,10 +50,10 @@ def solve(highbit:int, p_extra:int, progress:array, depth:int=0):
     depth_limit = num_words - 1
     for dx in range(p_extra + 1):
         least = highbit.bit_length() - 1
-        for pack in lc_letter[least]:
+        for pack in word_pack[least]:
             required = decompress(pack)
             if (highbit & required) == required:
-                for mask in lc_letter[least][pack]:
+                for mask in word_pack[least][pack]:
                     if (mask & highbit) == mask:
                         progress[depth] = mask
                         if depth >= depth_limit:
@@ -64,18 +62,23 @@ def solve(highbit:int, p_extra:int, progress:array, depth:int=0):
                             solve(highbit & ~mask, p_extra - dx, progress, depth + 1)
         highbit ^= 1 << least
 
-def main_find():
+def run_find():
     """process a list of words to find groups of words of the same length with each having unique letters"""
-    ct = 0
+    global word_pack
+    extra = num_letters - (word_size * num_words)
+    show(f"num extra letters = {extra}\n")
+    word_pack = [{} for _ in range(num_letters)]
+
+    wct = 0
     least = 0
     wf = json.load( open(WORD_FILE) )
     for word in wf:
         if len(word) == word_size:
-            ct += 1
+            wct += 1
             mask = 0
             for lett in word:
                 lx = ordered_letters.find(lett)
-                if 0 > lx >= num_letters:
+                if lx < 0 or lx >= num_letters:
                     break
                 bx = 1 << lx
                 if mask & bx:
@@ -84,15 +87,15 @@ def main_find():
             else:
                 least = mask.bit_length() - 1
                 pack = compress(mask)
-                wordnames.setdefault( mask, set() ).add(word)
-                lc_letter[least].setdefault(pack, set()).add(mask)
+                word_names.setdefault( mask, set() ).add(word)
+                word_pack[least].setdefault( pack, set() ).add(mask)
 
+    show(f"found {wct} words.")
     # DEBUG
-    print(f"found {ct} words.")
-    print(f"len wordnames = {len(wordnames)}")
-    print(f"last 'least' = {least}")
-    print(f"len lc_letter[least] = {len(lc_letter[least])}")
-    print(f"lc_letter[least] = {lc_letter[least]}")
+    lgr.debug(f"len word_names = {len(word_names)}")
+    lgr.debug(f"last 'least' = {least}")
+    lgr.debug(f"len word_pack[least] = {len(word_pack[least])}")
+    lgr.debug(f"word_pack[least] = {word_pack[least]}")
 
     initializer = DEFAULT_INITIALIZER
     if num_words == 4:
@@ -101,60 +104,70 @@ def main_find():
         initializer = (0, 0, 0)
     elif num_words == MIN_NUMWORDS:
         initializer = (0, 0)
-    solve((1 << num_letters) - 1, extra, array.array('L', initializer))
+    solve( (1 << num_letters) - 1, extra, array.array('L', initializer) )
 
     # sample some of the output
-    print(f"\nsolutions:")
+    show(f"\nsolutions:")
     nx = 0
     for index in output.keys():
-        print(output[index])
+        show(output[index])
         nx += 1
-        if nx > 30:
+        if nx == 32:
+            if count > 32:
+                show("etc...")
             break
-    print(f"total groups = {count}")
+    show(f"total groups = {count}")
 
-    print(f"\nelapsed time = {time.perf_counter() - start}")
-    if save_option.upper()[0] == 'Y' and len(save_option) <= 4:
+    show(f"\nsolve and display elapsed time = {time.perf_counter() - start}")
+    if save_option:
         save_to_json(f"{word_size}x{num_words}f{num_letters}_words", output)
 
 
+def process_args():
+    arg_parser = ArgumentParser(description="get the save-to-file, word size, required letters and possible letters options", prog="ideal_words.py")
+    # optional arguments
+    arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
+    arg_parser.add_argument('-w', '--wordsize', type=int, default=MIN_WORDSIZE, help="number of letters in each found word")
+    arg_parser.add_argument('-n', '--numwords', type=int, default=MAX_NUMWORDS, help="number of words in each found group")
+    arg_parser.add_argument('-l', '--numletters', type=int, default=MAX_NUMLETTERS, help="number of letters to search for each word")
+    return arg_parser
+
+def prep_find(argl:list) -> (bool, int, int):
+    args = process_args().parse_args(argl)
+
+    lgr.info("START LOGGING")
+    show(f"save option = '{args.save}'")
+
+    show(f"requested word size = {args.wordsize}")
+    word_sz = args.wordsize if MIN_WORDSIZE <= args.wordsize <= MAX_WORDSIZE else MIN_WORDSIZE
+    show(f"requested number of words = {args.numwords}")
+    num_wds = args.numwords if MIN_NUMWORDS <= args.numwords <= MAX_NUMWORDS else MAX_NUMWORDS
+    show(f"requested number of letters = {args.numletters}")
+    num_letts = args.numletters if MIN_NUM_LETTERS <= args.numletters <= MAX_NUMLETTERS else MAX_NUMLETTERS
+
+    return args.save, word_sz, num_wds, num_letts
+
+
 if __name__ == '__main__':
-    print(f"argv = {argv}")
-    save_option = "No"
-    if len(argv) > 1 and argv[1].isalpha():
-        save_option = argv[1]
-    print(f"save option = '{save_option}'")
+    log_control = MhsLogger(get_base_filename(__file__))
+    lgr = log_control.get_logger()
+    show = log_control.show
 
-    word_size = DEFAULT_WORDSIZE
-    if len(argv) > 2:
-        request = int(argv[2])
-        if MAX_WORDSIZE >= request > DEFAULT_WORDSIZE:
-            print(f"requested word size = {request}")
-            word_size = request
-    num_words = MAX_NUMWORDS
-    if len(argv) > 3:
-        request = int(argv[3])
-        print(f"number of words requested = {request}")
-        if MAX_NUMWORDS > request >= MIN_NUMWORDS:
-            num_words = request
-    ordered_letters = ORDERED_LETTERS
-    if len(argv) > 4:
-        request = int(argv[4])
-        print(f"number of letters requested = {request}")
-        if MIN_NUM_LETTERS <= request < DEFAULT_NUMLETTERS:
-            ordered_letters = ORDERED_LETTERS[:request]
+    count = 0
+    word_names = {}
+    word_pack = []
+    output = {}
 
+    save_option, word_size, num_words, num_letters = prep_find(argv[1:])
+
+    ordered_letters = ORDERED_LETTERS[:num_letters]
+    show(f"letters to use are: '{ordered_letters}'")
     # make sure the parameters for size of words & number of words and letters are safe and sensible
-    num_letters = len(ordered_letters)
-    print(f"letters to use are: '{ordered_letters}'")
     if word_size * num_words > num_letters:
-        word_size = DEFAULT_WORDSIZE
-        num_words = num_letters // DEFAULT_WORDSIZE
-    print(f"find {num_words} 'uniquely lettered' words each with {word_size} letters.")
-    extra = num_letters - (word_size * num_words)
-    print(f"num extra letters = {extra}\n")
-    lc_letter = [{} for f in range(num_letters)]
+        word_size = MIN_WORDSIZE
+        num_words = num_letters // word_size
+    show(f"Find groups of {num_words} 'uniquely lettered' words each with {word_size} letters.")
 
-    main_find()
-    print(f"\nelapsed time = {time.perf_counter() - start}")
+    run_find()
+    show(f"\nfinal elapsed time = {time.perf_counter() - start}")
     exit()
