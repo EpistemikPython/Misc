acfeb3d8368cdbd2f94aadd58f8f4a2350ba9e7d
epistemik <epistemik@gmail.com>
2023-12-15 17:07:57 -0500

    use global vars for parameters; minor updates

--- words/spellingbee_words.py
+++ words/spellingbee_words.py
@@ -9,7 +9,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2023-12-08"
+__updated__ = "2023-12-15"
 
 import time
 import json
@@ -26,14 +26,14 @@ GROUP_SIZE = len(DEFAULT_OUTER_LETTERS)
 DEFAULT_REQD_LETTER = "I"
 MIN_WORD_SIZE = 4
 
-def run_sb(save_option, required, group):
+def run_sb():
     """find all words from a list that fulfill the specified spelling bee requirements"""
     solutions = []
-    sbw = json.load( open(WORD_FILE) )
-    for item in sbw:
+    wdf = json.load( open(WORD_FILE) )
+    for item in wdf:
         if len(item) >= MIN_WORD_SIZE:
             for letter in item:
-                if letter not in group + required:
+                if letter not in outers + required:
                     break
             else:
                 if required in item:
@@ -45,7 +45,7 @@ def run_sb(save_option, required, group):
     show(f"solutions:")
     for val in solutions:
         pg = True
-        for lett in group:
+        for lett in outers:
             if lett not in val:
                 pg = False
                 break
@@ -58,7 +58,7 @@ def run_sb(save_option, required, group):
 
     show(f"\nsolve and display elapsed time = {time.perf_counter() - start}")
     if save_option:
-        save_to_json(f"{required}-{group}_spellbee_words", solutions)
+        save_to_json(f"{required}-{outers}_spellbee_words", solutions)
 
 
 def process_args():
@@ -73,18 +73,18 @@ def process_args():
 def prep_sb(argl:list) -> (bool, str, str):
     args = process_args().parse_args(argl)
 
-    lgr.warning("START LOGGING")
+    lgr.info("START LOGGING")
     show(f"save option = '{args.save}'")
 
-    required = args.required[0].upper() if args.required.isalpha() else DEFAULT_REQD_LETTER
+    require = args.required[0].upper() if args.required.isalpha() else DEFAULT_REQD_LETTER
     # make sure the required letter is not in the outer letters?
-    show(f"required letter = {required}")
+    show(f"required letter = {require}")
 
-    outer = args.outer.upper() if args.outer.isalpha() and len(args.outer) == GROUP_SIZE else DEFAULT_OUTER_LETTERS
+    others = args.outer.upper() if args.outer.isalpha() and len(args.outer) == GROUP_SIZE else DEFAULT_OUTER_LETTERS
     # make sure all the outer letters are different?
-    show(f"outer letters = {outer}")
+    show(f"outer letters = {others}")
 
-    return args.save, required, outer
+    return args.save, require, others
 
 
 if __name__ == '__main__':
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     lgr = log_control.get_logger()
     show = log_control.show
 
-    save, reqd, others = prep_sb(argv[1:])
-    run_sb( save, reqd, others )
+    save_option, required, outers = prep_sb(argv[1:])
+    run_sb()
     show(f"\nfinal elapsed time = {time.perf_counter() - start}")
     exit()
