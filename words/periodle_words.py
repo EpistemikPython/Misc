836a6acf00078f3218c89fc23cc217c4e0bfad26
epistemik <epistemik@gmail.com>
2023-12-23 09:22:16 -0500

    add MhsLogger; add ArgumentParser with parameters for save-to-json-file, symbols file name, words file name; &

--- words/periodle_words.py
+++ words/periodle_words.py
@@ -1,7 +1,7 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# periodle_words.py -- process a words file to find periodle (5-10 letters made up of 5 element symbols) words and save to a separate file
+# periodle_words.py -- process a words file to find periodle words (5-10 letters using element symbols) and save to a separate file
 #
 # Copyright (c) 2023 Mark Sattolo <epistemik@gmail.com>
 
@@ -9,23 +9,25 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-12-18"
-__updated__ = "2023-12-22"
+__updated__ = "2023-12-23"
 
+import os
 import json
 import time
+from argparse import ArgumentParser
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
-from mhsUtils import save_to_json
+from mhsUtils import save_to_json, get_base_filename, JSON_LABEL
+from mhsLogging import MhsLogger
 
 start = time.perf_counter()
-WORD_FILE = "test2.json" # "periodle_words.json"
-ELEMENT_FILE = "periodic_table.json"
+WORD_JSON_FILE = "test2" # "periodle_words"
+ELEMENT_JSON_FILE = "periodic_table"
 BLANK = '_'
 
 def solve():
-    print(f"Word file = {WORD_FILE}")
     wd_ct = 0
-    wdf = json.load( open(WORD_FILE) )
+    wdf = json.load( open(word_file) )
     for item in wdf:
         wd_ct += 1
         wd_len = len(item)
@@ -45,8 +47,8 @@ def solve():
             dg_reqd = wd_len - 5
             dg_ct = 0
             prev_lett = BLANK
-            # pop all the accepted symbols on a stack
-            # if reach an end: REWIND to the most recent single and use it and the next letter as a double
+            # pop each accepted symbol on a stack
+            # if reach a failure point: REWIND to the most recent single and use it and the next letter as a double
             # and resume from there...
             while step < wd_len and not drop:
                 lett = item[step].upper()
@@ -95,64 +97,86 @@ def solve():
                     step += 1
             if not drop:
                 prd_list.append(item.upper())
-                print(stack)
-    print(f"{wd_ct} words in word file.")
+                lgr.info(stack)
+    show(f"{wd_ct} words in word file.")
 
 def run_periodle():
-    """process a words file to find periodle (5-10 letter) words and save to a separate file"""
-    print(f"save option = '{save_option}'\n")
+    """process a words file to find periodle words (5-10 letters using element symbols) and save to a separate file"""
 
     solve()
-
     num_wd = len(prd_list)
-    print(f"periodle word count = {num_wd}\n")
+    show(f"periodle word count = {num_wd}\n")
+
     ni = 0
-    nli = num_wd // 11 if num_wd > 150 else 5 if num_wd > 30 else 1
+    nli = num_wd // 32 if num_wd > 150 else 12 if num_wd > 30 else 1
     # display a selection of the output
-    print("sample output:")
+    show("sample output:")
     for word in prd_list:
         if ni % nli == 0:
-            print(word)
+            show(word)
         ni += 1
 
-    print(f"\nsolve and display elapsed time = {time.perf_counter() - start}")
-    if save_option.upper()[0] == 'Y':
-        save_to_json("periodle_words", prd_list)
+    show(f"\nsolve and display elapsed time = {time.perf_counter() - start}")
+    if save_option:
+        save_to_json(f"periodle-words_f-{get_base_filename(word_file)}", prd_list)
 
 def get_symbols():
-    elf = json.load( open(ELEMENT_FILE) )
-    for sect in elf:
+    symf = json.load( open(symbol_file) )
+    for sect in symf:
         if sect == "single":
-            for symbol in elf[sect]:
+            for symbol in symf[sect]:
                 singles.append(symbol)
         elif sect == "double":
-            for symbol in elf[sect]:
+            for symbol in symf[sect]:
                 doubles.append(symbol.upper())
-    print(f"singles = {singles}")
-    print(f"doubles = {doubles}")
+    lgr.debug(f"singles = {singles}")
+    lgr.debug(f"doubles = {doubles}")
+
+def process_args():
+    arg_parser = ArgumentParser(description="get the save-to-file, required letter and outer letters options", prog="python3.10 spellingbee_words.py")
+    # optional arguments
+    arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
+    arg_parser.add_argument('-y', '--symbolfile', type=str, default=WORD_JSON_FILE,
+                                help= "JSON file name with the symbols to use (.json added to file name)")
+    arg_parser.add_argument('-w', '--wordfile', type=str, default=ELEMENT_JSON_FILE,
+                                help= "JSON file name with the words to test (.json added to file name)")
+    return arg_parser
+
+def prep_sb(argl:list) -> (bool, str, str):
+    args = process_args().parse_args(argl)
+
+    lgr.info("START LOGGING")
+    show(f"save option = '{args.save}'")
+
+    symbols = args.symbolfile if args.symbolfile.isprintable() else ELEMENT_JSON_FILE
+    symbols += (os.extsep + JSON_LABEL)
+    show(f"symbol file = {symbols}")
+
+    words = args.wordfile if args.wordfile.isprintable() else WORD_JSON_FILE
+    words += (os.extsep + JSON_LABEL)
+    show(f"word file = {words}")
+
+    return args.save, symbols, words
 
 
 if __name__ == '__main__':
-    # logging
-    # ArgParser
-    
-    save_option = "No"
-    if len(argv) > 1 and argv[1].isalpha():
-        save_option = argv[1]
+    log_control = MhsLogger( get_base_filename(__file__) )
+    lgr = log_control.get_logger()
+    show = log_control.show
 
+    save_option, symbol_file, word_file = prep_sb(argv[1:])
     code = 0
+    singles = []
+    doubles = []
+    prd_list = []
     try:
-        singles = []
-        doubles = []
         get_symbols()
-
-        prd_list = []
         run_periodle()
     except KeyboardInterrupt:
-        print(">> User interruption.")
+        show(">> User interruption.")
     except Exception as ex:
-        print(f"Problem = '{repr(ex)}'")
+        show(f"Problem = '{repr(ex)}'")
         code = 66
 
-    print(f"\nfinal elapsed time = {time.perf_counter() - start}")
+    show(f"\nfinal elapsed time = {time.perf_counter() - start}")
     exit(code)
