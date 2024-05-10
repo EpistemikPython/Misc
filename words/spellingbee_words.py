c166da785bbcf14a2460c41acd5f39752210df71
epistemik <epistemik@gmail.com>
2024-05-10 17:30:04 -0400

    remove default letter choices; adjust argument description and extra error checking

--- words/spellingbee_words.py
+++ words/spellingbee_words.py
@@ -9,7 +9,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2024-01-12"
+__updated__ = "2024-05-08"
 
 import time
 import json
@@ -21,13 +21,12 @@ from mhsLogging import MhsLogger
 
 start = time.perf_counter()
 WORD_FILE = "input/scrabble-plus.json"
-DEFAULT_OUTER_LETTERS = "DARCON"
-GROUP_SIZE = len(DEFAULT_OUTER_LETTERS)
-DEFAULT_REQD_LETTER = "V"
+OUTER_SIZE = 6
 MIN_WORD_SIZE = 4
+DEFAULT_SKIP = 4
 
 def run():
-    """find all words from a list that fulfill the specified spelling bee requirements"""
+    """from a word list file, find all words that fulfill the specified spelling bee requirements"""
     solutions = []
     wdf = json.load( open(WORD_FILE) )
     for item in wdf:
@@ -41,10 +40,10 @@ def run():
 
     show(f"solution count = {len(solutions)}")
     # check for pangrams and print the solutions
-    skip = 1 if len(solutions) < 30 else 5
+    skip = 1 if len(solutions) < 50 else DEFAULT_SKIP
     ct = 0
     pgct = 0
-    show(f"{'Sample' if skip == 5 else 'All'} solutions:")
+    show(f"{'Sample of' if skip == DEFAULT_SKIP else 'ALL'} solutions:")
     for val in solutions:
         pg = True
         for lett in outers:
@@ -67,11 +66,11 @@ def run():
         show(f"Save output to file '{save_name}'.")
 
 def set_args():
-    arg_parser = ArgumentParser(description="get the save-to-file, required letter and outer letters options", prog="python3 spellingbee_words.py")
+    arg_parser = ArgumentParser(description="get the save-to-file, central and outer letters options", prog="python3 spellingbee_words.py")
     # optional arguments
     arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
-    arg_parser.add_argument('-r', '--required', type=str, default=DEFAULT_REQD_LETTER, help="this ONE letter MUST be in each word")
-    arg_parser.add_argument('-o', '--outer', type=str, default=DEFAULT_OUTER_LETTERS, help="SIX other POSSIBLE letters in the words")
+    arg_parser.add_argument('-c', '-r', '--central', type=str, required=True, help="this ONE letter MUST be in each word")
+    arg_parser.add_argument('-o', '-p', '--outer', type=str, required=True, help="SIX other POSSIBLE letters in the words")
     return arg_parser
 
 def prep_args(argl:list) -> (bool, str, str):
@@ -80,27 +79,26 @@ def prep_args(argl:list) -> (bool, str, str):
     lgr.info("START LOGGING")
     show(f"save option = '{args.save}'")
 
-    if args.required.isalpha() and len(args.required) == 1:
-        require = args.required.upper()
-        show(f"required letter = {require}")
+    central = args.central.upper()
+    if central.isalpha() and len(central) == 1:
+        show(f"central letter = {central}")
     else:
-        require = DEFAULT_REQD_LETTER
-        show(f"INVALID required letter parameter '{args.required}'! Using DEFAULT '{DEFAULT_REQD_LETTER}'.")
+        show(f"INVALID central letter '{central}'!")
+        raise Exception("bad central letter")
 
     # make sure all the outer letters are different?
     outer = args.outer.upper()
-    if outer.isalpha() and len(outer) == GROUP_SIZE:
-        if require in outer:
-            others = outer.replace(require, 'X')
-            show(f"INVALID outer letters '{args.outer}'! Required letter '{require}' CANNOT be in outers! >> Replace with 'X': outers = '{others}'")
+    if outer.isalpha() and len(outer) == OUTER_SIZE:
+        if central in outer:
+            show(f"INVALID outer letters '{outer}'! Required letter '{central}' CANNOT be in outers!")
+            raise Exception("central letter in outers")
         else:
-            others = outer
-            show(f"outer letters = {others}")
+            show(f"outer letters = {outer}")
     else:
-        show(f"INVALID outer letters '{args.outer}'!! Using DEFAULT '{DEFAULT_OUTER_LETTERS}'.")
-        others = DEFAULT_OUTER_LETTERS
+        show(f"INVALID outer letters '{outer}'!!")
+        raise Exception("bad outer letters")
 
-    return args.save, require, others
+    return args.save, central, outer
 
 
 if __name__ == '__main__':
