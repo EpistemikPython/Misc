a0ad3ab0fec83974537e6b3f0a3a7f8b31fe3c16
epistemik <epistemik@gmail.com>
2024-06-04 09:09:03 -0400

    see the dictionary details inside a json file and find the listed words and save to a new file

--- words/find_sb_json.py
+++ words/find_sb_json.py
@@ -9,7 +9,7 @@ __author__ = "Mark Sattolo"
 __author_email__ = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2024-06-01"
-__updated__ = "2024-06-02"
+__updated__ = "2024-06-04"
 
 import time
 import json
@@ -18,18 +18,20 @@ path.append("/home/marksa/git/Python/utils")
 from mhsUtils import save_to_json
 
 start = time.perf_counter()
-TEST_FILE = "/home/marksa/Documents/Words/SpellBeeWords.json"
+WORD_FILE = "/home/marksa/Documents/Words/SpellBeeWords.json"
+WORD_KEY = "answers"
+DATA_KEY = "data"
 
 def run():
     """see the dictionary details inside a json file and find the listed words and save to a new file"""
     word_list = []
-    sbd = json.load( open(TEST_FILE) )
+    sbd = json.load( open(WORD_FILE) )
     for item in sbd:
         current_dict = sbd[item]
-        if 'answers' in current_dict.keys():
-            words = current_dict['answers']
+        if WORD_KEY in current_dict.keys():
+            words = current_dict[WORD_KEY]
         else:
-            words = current_dict['data']['answers']
+            words = current_dict[DATA_KEY][WORD_KEY]
         print(f"{item}: {words}")
         for wd in words:
             lowd = wd.lower()
@@ -47,7 +49,7 @@ if __name__ == '__main__':
         print(">> User interruption.")
         code = 13
     except Exception as ex:
-        print(f"Problem: '{repr(ex)}'")
+        print(f"Problem: {repr(ex)}")
         code = 66
 
     print(f"\nfinal elapsed time = {time.perf_counter() - start}")
