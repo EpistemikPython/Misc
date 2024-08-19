d845dd6c66a77c20ac5ff574bb945d4c42bb4e84
epistemik <epistemik@gmail.com>
2024-08-19 13:58:47 -0400

    add file option to args; improve args help; simplify logging

--- words/find_length_words.py
+++ words/find_length_words.py
@@ -1,22 +1,24 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# choose_words.py -- process a list of words to find words of the specified lengths and optionally save to a JSON file
+# find_length_words.py
+#   -- process a list of words to find words of the specified lengths and optionally save to a JSON file
 #
-# Copyright (c) 2023 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
 
 __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2024-01-11"
+__updated__ = "2024-08-18"
 
 import time
 import json
+import os.path as osp
 from argparse import ArgumentParser
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
-from mhsUtils import save_to_json, get_base_filename
+from mhsUtils import save_to_json, JSON_LABEL, get_base_filename
 from mhsLogging import MhsLogger
 
 start = time.perf_counter()
@@ -26,9 +28,9 @@ MIN_LENGTH = 2
 MAX_LENGTH = 15
 
 def run():
-    """process a list of words to find words of the specified length(s) and save to a json file"""
+    """process a list of words to find words of the specified lengths and optionally save to a JSON file"""
     newlist = []
-    wdf = json.load(open(DEFAULT_WORD_FILE))
+    wdf = json.load( open(file_name) )
     for item in wdf:
         if lower <= len(item) <= upper:
             newlist.append(item)
@@ -46,21 +48,29 @@ def run():
     if save_option:
         save_name = f"{lower}-{upper}_letter_words" if lower != upper else f"{lower}-letter_words"
         save_to_json(save_name, newlist)
-        show(f"Save output to file '{save_name}'.")
+        show(f"Save output to file '{JSON_LABEL}{osp.sep}{save_name}'.")
 
 def set_args():
-    arg_parser = ArgumentParser(description="process a list of words to find words of the specified length(s)", prog="python3 choose_words.py")
+    arg_parser = ArgumentParser(description="process a list of words to find words of the specified lengths", prog=f"python3 {argv[0]}")
     # optional arguments
     arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
-    arg_parser.add_argument('-l', '--lower', type=int, default=DEFAULT_LENGTH, help="minimum number of letters in the words")
-    arg_parser.add_argument('-u', '--upper', type=int, default=MAX_LENGTH, help="maximum number of letters in the words")
+    arg_parser.add_argument('-f', '--file', type=str, default = DEFAULT_WORD_FILE,
+                            help = f"path to alternate file with list of all acceptable words; DEFAULT = {DEFAULT_WORD_FILE}.")
+    arg_parser.add_argument('-l', '--lower', type=int, default = DEFAULT_LENGTH,
+                            help = f"minimum number of letters in the words; DEFAULT = {DEFAULT_LENGTH}; MIN = {MIN_LENGTH}")
+    arg_parser.add_argument('-u', '--upper', type=int, default = MAX_LENGTH,
+                            help = f"maximum number of letters in the words; DEFAULT = {MAX_LENGTH}; MAX = {MAX_LENGTH}")
     return arg_parser
 
 def prep_args(argl:list) -> (bool, int, int):
     args = set_args().parse_args(argl)
 
-    lgr.info("START LOGGING")
+    lgr.logl("START LOGGING")
     show(f"save option = '{args.save}'")
+
+    if not osp.isfile(args.file):
+        raise Exception(f"File path '{args.file}' does not exist.")
+    show(f"Using word file '{args.file}'")
 
     low = args.lower if MIN_LENGTH <= args.lower <= MAX_LENGTH else DEFAULT_LENGTH
     show(f"lower limit = {low}")
@@ -68,15 +78,14 @@ def prep_args(argl:list) -> (bool, int, int):
     up = args.upper if low <= args.upper <= MAX_LENGTH else MAX_LENGTH
     show(f"upper limit = {up}")
 
-    return args.save, low, up
+    return args.save, args.file, low, up
 
 
 if __name__ == '__main__':
-    log_control = MhsLogger(get_base_filename(__file__))
-    lgr = log_control.get_logger()
-    show = log_control.show
+    lgr = MhsLogger( get_base_filename(__file__) )
+    show = lgr.show
 
-    save_option, lower, upper = prep_args(argv[1:])
+    save_option, file_name, lower, upper = prep_args(argv[1:])
     run()
     show(f"\nfinal elapsed time = {time.perf_counter() - start}")
     exit()
