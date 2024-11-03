2df3bc9a0510cc61ed30742f90512c795f44a0c7
epistemik <epistemik@gmail.com>
2024-11-03 10:55:31 -0500

    add input word to json file save name; change main exception handling to standard format

--- words/jumble.py
+++ words/jumble.py
@@ -10,7 +10,7 @@ __author_name__    = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2024-10-27"
-__updated__ = "2024-10-29"
+__updated__ = "2024-11-03"
 
 import logging
 import time
@@ -30,7 +30,7 @@ def run():
     for item in results:
         lgr.log(log_lev, item)
     if save_option:
-        outfile_name = save_to_json(get_base_filename(__file__), results)
+        outfile_name = save_to_json(f"{input_word.upper()}-{get_base_filename(__file__)}", results)
         lgr.info(f"Saved results to: {outfile_name}")
 
 def jumbler(letts:str, storage:list):
@@ -64,7 +64,7 @@ def set_args():
     # optional arguments
     arg_parser.add_argument('-s', '--save', action = "store_true", default = False,
                             help = "Save the output to a JSON file; DEFAULT = False.")
-    arg_parser.add_argument('-l', '--loglev', type = str, default = logging.getLevelName(DEFAULT_LOG_LEVEL),
+    arg_parser.add_argument('-l', '--loglevel', type = str, default = logging.getLevelName(DEFAULT_LOG_LEVEL),
                             help = f"level for optional logging; DEFAULT = '{logging.getLevelName(DEFAULT_LOG_LEVEL)}'.")
     arg_parser.add_argument('-i', '--input', type = str, default = DEFAULT_INPUT_WORD,
                             help = f"word to jumble; DEFAULT = '{DEFAULT_INPUT_WORD}'.")
@@ -72,29 +72,30 @@ def set_args():
 
 def get_args(argl:list) -> (bool, str, str):
     args = set_args().parse_args(argl)
-    glv = get_level(args.loglev)
+    glv = get_level(args.loglevel)
     inputw = args.input if args.input.isalpha() else DEFAULT_INPUT_WORD
     return args.save, glv if glv else DEFAULT_LOG_LEVEL, inputw
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    lgr = get_simple_logger(f"simple_{get_base_filename(__file__)}", file_handling = False)
+    basename = get_base_filename(__file__)
+    lgr = get_simple_logger(f"simple_{basename}", file_handling = False)
     code = 0
     try:
         save_option, log_lev, input_word = get_args(argv[1:])
-        log_control = MhsLogger(get_base_filename(__file__), con_level = DEFAULT_LOG_LEVEL)
+        log_control = MhsLogger(basename, con_level = DEFAULT_LOG_LEVEL)
         lgr = log_control.get_logger()
         lgr.info(f"save option = '{save_option}'; log level = '{logging.getLevelName(log_lev)}'; input word = '{input_word}'")
         run()
-    except KeyboardInterrupt:
-        lgr.exception(">> User interruption.")
+    except KeyboardInterrupt as mki:
+        lgr.exception(mki)
         code = 13
-    except ValueError:
-        lgr.exception(">> Value Error.")
+    except ValueError as mve:
+        lgr.exception(mve)
         code = 27
     except Exception as mex:
-        lgr.exception(f">> PROBLEM: {repr(mex)}")
+        lgr.exception(mex)
         code = 66
 
     lgr.info(f"Elapsed time = {time.perf_counter() - start} seconds")
