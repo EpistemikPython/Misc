546f989918fdb1830ac4f87892a4502183012a26
epistemik <epistemik@gmail.com>
2024-11-03 10:46:32 -0500

    save all entries in lower case; improve ArgumentParser argument names; change main exception handling to standard format; simplify logging

--- words/get_spellbee_words.py
+++ words/get_spellbee_words.py
@@ -10,10 +10,11 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2024-09-30"
-__updated__ = "2024-10-01"
+__updated__ = "2024-11-02"
 
 import time
 from argparse import ArgumentParser
+from os.path import basename
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
 from mhsUtils import osp, get_base_filename, get_filename, save_to_json
@@ -35,54 +36,50 @@ def run():
             elif END_KEY in line:
                 search_state = False
             elif search_state:
-                ansline = line.strip('," \n')
+                ansline = line.strip('," \n').lower()
                 if ansline not in results:
                     lgr.debug(f"add word '{ansline}' to results.")
                     results.append(ansline)
     if save_option:
-        outfile_name = save_to_json(get_base_filename(__file__), results)
+        outfile_name = save_to_json(basename, results)
         lgr.info(f"\nSaved results to: {outfile_name}")
 
 def set_args():
     arg_parser = ArgumentParser(description = "from a SpellingBee results JSON file, get all answers and save to a new JSON file",
                                 prog = f"python3 {get_filename(argv[0])}")
     # optional arguments
-    arg_parser.add_argument('-s', '--save', action = "store_false", default = True,
-                            help = "Save the output to a new JSON file; DEFAULT = True.")
-    arg_parser.add_argument('-i', '--input', type = str, default = DEFAULT_INPUT_FILE,
+    arg_parser.add_argument('-n', '--nosave', action = "store_true", default = False,
+                            help = "DO NOT save the output to a new JSON file; DEFAULT = False.")
+    arg_parser.add_argument('-f', '--file', type = str, metavar = "PATH", default = DEFAULT_INPUT_FILE,
                             help = f"path to a SpellingBee results JSON file with words to get; DEFAULT = '{DEFAULT_INPUT_FILE}'.")
     return arg_parser
 
 def get_args(argl:list) -> (bool, str, str):
     args = set_args().parse_args(argl)
-
-    loglev = DEFAULT_LOG_LEVEL
-
-    lgr.log(loglev, f"save option = '{args.save}'")
-
-    inputf = args.input if osp.isfile(args.input) else DEFAULT_INPUT_FILE
-    lgr.log(loglev, f"input file = '{inputf}'")
-
-    return args.save, inputf
+    lgr.info(f"save option = '{not args.nosave}'")
+    infile = args.file if osp.isfile(args.file) else DEFAULT_INPUT_FILE
+    lgr.info(f"JSON input file = '{infile}'")
+    return (not args.nosave), infile
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    log_control = MhsLogger( get_base_filename(__file__), con_level = DEFAULT_LOG_LEVEL )
+    basename = get_base_filename(__file__)
+    log_control = MhsLogger(basename, con_level = DEFAULT_LOG_LEVEL )
     lgr = log_control.get_logger()
     code = 0
     try:
         save_option, input_file = get_args(argv[1:])
         run()
-    except KeyboardInterrupt:
-        lgr.exception(">> User interruption.")
+    except KeyboardInterrupt as mki:
+        lgr.exception(mki)
         code = 13
-    except ValueError:
-        lgr.exception(">> Value Error.")
+    except ValueError as mve:
+        lgr.exception(mve)
         code = 27
     except Exception as mex:
-        lgr.exception(f">> PROBLEM: {repr(mex)}")
+        lgr.exception(mex)
         code = 66
 
-    lgr.info(f"\nfinal elapsed time = {time.perf_counter() - start} seconds")
+    lgr.info(f"\nElapsed time = {time.perf_counter() - start} seconds")
     exit(code)
