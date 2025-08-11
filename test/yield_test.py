337846bd8620822cf38834f7724bf23a83461390
epistemik <epistemik@gmail.com>
2025-08-11 11:44:07 -0400

    various tests

--- test/yield_test.py
+++ test/yield_test.py
@@ -1,30 +1,58 @@
+import sys
 
-import pydoc
+
+# import pydoc
 
 def rconcat(a, b):
     """use 'return' in a loop"""
     for ia in a:
-        return ia
+        print(ia)
     for ib in b:
-        return ib
+        print(ib)
+    return None
 
-def yconcat(a, b):
+
+def yconcat(a, b, c):
     """using 'yield' in a  loop"""
-    for ia in a:
-        yield ia
-    for ib in b:
-        yield ib
+    try:
+        for ia in range(a):
+            yield ia
+        for ib in range(b,c):
+            yield ib
+        # for xb in range(sys.maxsize):
+        yield "END"
+    except ValueError:
+        print("V-FINISHED!")
+    except OverflowError:
+        print("O-FINISHED!")
+    except Exception:
+        print("E-FINISHED!")
 
 
 # for r in rconcat(range(5), range(9, 13)):
 #     print(r)
 print( rconcat(range(5), range(13)) )
 
+print('==============================================')
+
+# Notice the use of the yield statement, instead of return. We can now use something like:
+ytester = yconcat(5, 9, 13)
+print(f"type(yconcat) = {type(yconcat)}")
+print(f"type(ytester) = {type(ytester)}")
+
+print(next(ytester))
 print('break')
+for r in range(13):
+    # print(ytester.send(7))
+    print(next(ytester))
+print('break')
+print(ytester.send((3,6,9)))
 
 # Notice the use of the yield statement, instead of return. We can now use something like
-for y in yconcat(range(5), range(9, 13)):
-    print(y)
+# for y in yconcat(range(5), range(9, 13)):
+#     print(y)
 
 # help(yconcat)
-pydoc.writedoc('yield_test')
+# pydoc.writedoc('yield_test')
+
+exit()
