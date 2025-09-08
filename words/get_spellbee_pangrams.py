1bc580b6af811c436228b0fce9f09dc5284a8eb8
epistemik <epistemik@gmail.com>
2025-09-08 07:45:46 -0400

    add command line argument for testing; extra processing to remove more plurals, simple past and 'ing' terms from pangrams

--- words/get_spellbee_pangrams.py
+++ words/get_spellbee_pangrams.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 #
 # get_spellbee_pangrams.py
-#   -- from a spelling-bee words file, get all pangrams and save to a JSON file
+#   -- from a spelling-bee words file, get pangrams and save to a JSON file
 #
 # Copyright (c) 2025 Mark Sattolo <epistemik@gmail.com>
 
@@ -10,7 +10,7 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2025-08-19"
-__updated__ = "2025-08-24"
+__updated__ = "2025-09-08"
 
 import time
 from argparse import ArgumentParser
@@ -19,8 +19,11 @@ path.append("/home/marksa/git/Python/utils")
 from mhsUtils import osp, save_to_json, get_base_filename, get_filename
 from mhsLogging import MhsLogger, DEFAULT_LOG_LEVEL
 
-DEFAULT_INPUT_FILE    = "input/spellbee_words.json"
+DEFAULT_INPUT_FILE = "input/spellbee_words.json"
+TEST_INPUT_FILE    = "input/spellbee_words_test.json"
 DEFAULT_OUTPUT_FOLDER = "./output"
+MIN_SPELLBEE_LENGTH = 4
+MIN_PANGRAM_LENGTH = 7
 
 def is_pangram(test_word:str) -> bool:
     # eliminate simple plurals
@@ -33,32 +36,61 @@ def is_pangram(test_word:str) -> bool:
         if lett not in unique:
             unique += lett
     # lgr.debug(f"unique = '{unique}'")
-    if len(unique) == 7:
+    if len(unique) == MIN_PANGRAM_LENGTH:
         lgr.debug(f">> {test_word} is a Pangram!")
         return True
     return False
 
 def run():
-    """from a spelling-bee words file, get all pangrams and save to a JSON file."""
+    """from a spelling-bee words file, get regular pangrams and save to a JSON file."""
     global previous_word
     base_words = []
     pangrams = []
     with open(input_file) as file_in:
         for line in file_in:
             # lgr.debug(line)
-            testword = line.strip('-_", \n')
-            if len(testword) >= 4:
+            testword = line.strip('-_", \n').upper()
+            if len(testword) >= MIN_SPELLBEE_LENGTH:
                 base_words.append(testword)
             if is_pangram(testword):
                 pangrams.append(testword)
             previous_word = testword
-    # find and remove other plurals and simple past
+    # find and remove other plurals and simple past and 'ing'
+    remove_words = []
+    for word in pangrams:
+        if word[-3:] == "IES" or word[-3:] == "ING":
+            remove_words.append(word)
+            continue
+        if word[-1] == 'S':
+            rootword = word[:-1]
+            if rootword in base_words:
+                remove_words.append(word)
+                continue
+            if rootword[-1] == 'E':
+                nextrootword = rootword[:-1]
+                if nextrootword in base_words:
+                    remove_words.append(word)
+                    continue
+        if word[-2:] == "ED":
+            rootword = word[:-1]
+            if rootword in base_words:
+                remove_words.append(word)
+                continue
+            rootword = word[:-2]
+            if rootword in base_words:
+                remove_words.append(word)
+
+    for item in remove_words:
+        if item in pangrams:
+            pangrams.remove(item)
 
     if save_option:
-        outfile_name = save_to_json("pangrams", pangrams)
+        outfile_name = save_to_json(f"pangrams{test}", pangrams)
         lgr.info(f"\nSaved all pangrams to: {outfile_name}")
-        outfile_name = save_to_json("all_spellbee_words", base_words)
-        lgr.info(f"\nSaved all spellbee words to: {outfile_name}")
+        outfile_name = save_to_json(f"all_spellbee_words{test}", base_words)
+        lgr.info(f"\nSaved ALL spellbee words to: {outfile_name}")
+        outfile_name = save_to_json(f"remove_words{test}", remove_words)
+        lgr.info(f"\nSaved all remove words to: {outfile_name}")
 
 def set_args():
     arg_parser = ArgumentParser(description = "from a spelling-bee words file, get all pangrams and save to a JSON file",
@@ -66,10 +98,13 @@ def set_args():
     # optional arguments
     arg_parser.add_argument('-s', '--save', action="store_true", default = False,
                             help = "Copy the found pangrams to a new file; DEFAULT = 'False'")
+    arg_parser.add_argument('-t', '--test', action="store_true", default = False,
+                            help = f"Use the test input file; DEFAULT = 'False'; TEST input file = '{TEST_INPUT_FILE}'.")
     arg_parser.add_argument('-i', '--input', type=str, default = DEFAULT_INPUT_FILE,
-                            help = f"path to a SpellingBee JSON file with words to check; DEFAULT = '{DEFAULT_INPUT_FILE}'.")
+                            help = f"path to an ALTERNATE SpellingBee JSON input file; DEFAULT input file = '{DEFAULT_INPUT_FILE}'.")
     arg_parser.add_argument('-o', '--output', type=str, default = DEFAULT_OUTPUT_FOLDER,
-                            help = f"output folder to store the produced JSON file; DEFAULT = '{DEFAULT_OUTPUT_FOLDER}'.")
+                            help = f"path to an ALTERNATE output folder to store the produced JSON file; "
+                                   f"DEFAULT folder = '{DEFAULT_OUTPUT_FOLDER}/'.")
     return arg_parser
 
 def get_args(argl:list):
@@ -79,14 +114,14 @@ def get_args(argl:list):
 
     lgr.log(loglev, f"save option = '{args.save}'")
 
-    inputf = args.input if osp.isfile(args.input) else DEFAULT_INPUT_FILE
+    inputf = TEST_INPUT_FILE if args.test else args.input if osp.isfile(args.input) else DEFAULT_INPUT_FILE
     lgr.log(loglev, f"input file = '{inputf}'")
 
     outputf = args.output if osp.isfile(args.output) else DEFAULT_OUTPUT_FOLDER
     if args.save:
         lgr.log(loglev, f"output folder = '{outputf}'")
 
-    return args.save, inputf, outputf
+    return args.save, inputf, outputf, "_test" if args.test else ""
 
 
 if __name__ == '__main__':
@@ -95,7 +130,7 @@ if __name__ == '__main__':
     lgr = log_control.get_logger()
     code = 0
     try:
-        save_option, input_file, output_folder = get_args(argv[1:])
+        save_option, input_file, output_folder, test = get_args(argv[1:])
         previous_word = ""
         run()
     except KeyboardInterrupt:
