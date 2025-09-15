8b8a140e1cc50371b54fb440a2377dfec966b227
epistemik <epistemik@gmail.com>
2025-09-14 21:45:29 -0400

    implement text file processing

--- words/get_words.py
+++ words/get_words.py
@@ -10,7 +10,7 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2025-09-12"
-__updated__ = "2025-09-13"
+__updated__ = "2025-09-14"
 
 import re
 import time
@@ -18,27 +18,50 @@ from argparse import ArgumentParser
 from os.path import basename
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
-from mhsUtils import osp, get_base_filename, get_filename, save_to_json
+from mhsUtils import osp, get_base_filename, get_filename, get_filetype, save_to_json
 from mhsLogging import MhsLogger, DEFAULT_LOG_LEVEL
 
-DEFAULT_INPUT_FILE = "./input/Wiktionary-37k.htm"
-TEST_INPUT_FILE    = "./input/Wiktionary-test.htm"
-DEFAULT_OUTPUT_FOLDER = "./"
-ANSWER_KEY = "answers"
-END_KEY    = "]"
+HTML_TYPE = "htm"
+DEFAULT_HTML_FILE = "./input/Wiktionary-37k.htm"
+TEST_HTML_FILE    = "./input/Wiktionary-test.htm"
+TEXT_TYPE = "txt"
+DEFAULT_TEXT_FILE = "./input/combined-wordlist-all_sort-uniq.txt"
+TEST_TEXT_FILE    = "./input/wordlist-test.txt"
 
 def run():
-    if input_type == "htm":
-        run_htm()
-    elif input_type == "txt":
-        run_txt()
+    if input_type == HTML_TYPE:
+        run_html()
+    elif input_type == TEXT_TYPE:
+        run_text()
     else:
         lgr.info("do nothing...")
 
-def run_txt():
-    pass
+def run_text():
+    first_data = []
+    with open(input_file) as file_in:
+        for it in file_in:
+            item = it.strip('-_",. \n').upper()
+            if 3 < len(item) < 22:
+                first_data.append(item)
+    second_data = first_data.copy()
+    third_data = []
+    # find and remove simple plurals
+    for word in second_data:
+        if word[-1] == 'S':
+            rootword = word[:-1]
+            if rootword in first_data:
+                continue
+            if rootword[-1] == 'E':
+                nextrootword = rootword[:-1]
+                if nextrootword in first_data:
+                    continue
+        third_data.append(word)
 
-def run_htm():
+    if save_option:
+        outfile_name = save_to_json("get_words_text", third_data)
+        lgr.info(f"\nSaved results to: {outfile_name}")
+
+def run_html():
     """From a words list file, get specified words and save to a new file."""
     out_data = []
     with open(input_file) as file_in:
@@ -51,7 +74,7 @@ def run_htm():
                 out_data.append(match_word)
 
     if save_option:
-        outfile_name = save_to_json(basename, out_data)
+        outfile_name = save_to_json("get_words_html", out_data)
         lgr.info(f"\nSaved results to: {outfile_name}")
 
 def set_args():
@@ -60,19 +83,18 @@ def set_args():
     # optional arguments
     arg_parser.add_argument('-n', '--nosave', action = "store_true", default = False,
                             help = "DO NOT save the output to a new JSON file; DEFAULT = False.")
-    arg_parser.add_argument('-t', '--type', type = str, default = "htm",
-                            help = f"Specify the TYPE of input file: 'txt' OR 'htm'; DEFAULT = 'htm'")
-    arg_parser.add_argument('-i', '--input', type = str, metavar = "inPATH", default = DEFAULT_INPUT_FILE,
-                            help = f"path to a word list file with words to get; DEFAULT = '{DEFAULT_INPUT_FILE}'.")
+    arg_parser.add_argument('-i', '--input', type = str, metavar = "inPATH", default = DEFAULT_TEXT_FILE,
+                            help = f"path to a word list file with words to get; DEFAULT = '{DEFAULT_TEXT_FILE}'.")
     return arg_parser
 
 def get_args(argl:list) -> tuple[bool, str, str]:
     args = set_args().parse_args(argl)
-    save_opt = ~args.nosave
+    save_opt = not args.nosave
     lgr.info(f"save option = '{save_opt}'")
-    infile = args.input if osp.isfile(args.input) else TEST_INPUT_FILE
+    itype = get_filetype(args.input).strip('.')
+    infile = args.input if osp.isfile(args.input) else TEST_TEXT_FILE
     lgr.info(f"input file = '{infile}'")
-    return save_opt, args.type, infile
+    return save_opt, itype, infile
 
 
 if __name__ == '__main__':
