ae2e0a09dab38797bcbfdac5431994097dee09e3
epistemik <epistemik@gmail.com>
2026-01-08 18:35:56 -0500

    keep count of number of word appearances of letters in addition to total frequency; convert to standard main and args; &

--- words/letter_frequencies.py
+++ words/letter_frequencies.py
@@ -1,91 +1,149 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# letter_frequencies.py -- get the frequency of each letter in words of specified length(s) from a word list
+# letter_frequencies.py
+# -- get the frequency and number of appearances of each letter in words of a specified length from a word list
 #
-# Copyright (c) 2023 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2026 Mark Sattolo <epistemik@gmail.com>
 
 __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2023-12-28"
+__updated__ = "2026-01-06"
 
+import string
 import time
 import json
 from argparse import ArgumentParser
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
-from mhsUtils import save_to_json
-from mhsLogging import MhsLogger, get_base_filename
+from mhsUtils import *
+from mhsLogging import *
 
 start = time.perf_counter()
-WORD_FILE = "input/scrabble-plus.json"
-ALL_LETTERS = "SEAORILTNUDYCPMHGBKFWVZJXQ"
-MIN_WORD_LEN = 5
-MAX_WORD_LEN = 15
+DEFAULT_WORD_FILE = "input/scrabble-plus.json"
+MIN_WORD_LEN     =  4
+DEFAULT_WORD_LEN =  5
+MAX_WORD_LEN     = 15
 
-def run_freqs():
-    """get the frequency of each letter in words of specified length(s) from a word file"""
-    freqs = dict.fromkeys(ALL_LETTERS, 0)
-    wct = lct = 0
-    wds = json.load( open(WORD_FILE) )
+def run():
+    """Get the frequency and number of appearances of each letter in words of a specified length from a word list."""
+    freqs = dict.fromkeys(string.ascii_uppercase, 0)
+    appearances = dict.fromkeys(string.ascii_uppercase, 0)
+    word_count = 0
+    letter_count = 0
+    wds = json.load( open(word_file) )
     for item in wds:
+        seen = []
         if min_size <= len(item) <= max_size:
             for letter in item:
                 freqs[letter] += 1
-                lct += 1
-            wct += 1
+                if letter not in seen:
+                    appearances[letter] += 1
+                seen.append(letter)
+                letter_count += 1
+            word_count += 1
 
-    show(f"word count = {wct}")
-    show(f"letter count = {lct}")
+    lgr.info(f"\tword count = {word_count}")
+    lgr.info(f"\tletter count = {letter_count}\n")
 
-    rev_freqs = {}
-    show(f"letter frequencies:")
+    lett_freqs = {}
+    total_freqs = 0
+    lgr.info(f"\n\tletter frequencies:")
     for key in freqs.keys():
-        show(f"\t{key}: {freqs[key]}")
+        lgr.info(f"\t{key}: {freqs[key]}")
+        total_freqs += freqs[key]
         # hopefully there are not two frequencies exactly the same
-        rev_freqs[freqs[key]] = key
+        lett_freqs[freqs[key]] = key
+    lgr.info(f"total letter frequencies = {total_freqs}")
 
     sorted_freqs = {}
-    show(f"\nsorted letter frequencies:")
-    for val in sorted(rev_freqs, reverse = True):
-        show(f"\t{rev_freqs[val]}: {val}")
-        sorted_freqs[rev_freqs[val]] = val
+    lgr.info(f"\n\tsorted letter frequencies:")
+    for val in sorted(lett_freqs, reverse = True):
+        lgr.info(f"\t{lett_freqs[val]}: {val}")
+        sorted_freqs[lett_freqs[val]] = val
 
-    show(f"\nsolve and display elapsed time = {time.perf_counter() - start}")
+    lett_appears = {}
+    total_appears = 0
+    lgr.info(f"\n\tletter appearances:")
+    for key in appearances.keys():
+        lgr.info(f"\t{key}: {appearances[key]}")
+        total_appears += appearances[key]
+        # hopefully there are not two appearance totals exactly the same
+        lett_appears[appearances[key]] = key
+    lgr.info(f"total letter appearances = {total_appears}")
+
+    sorted_appears = {}
+    lgr.info(f"\n\tsorted letter appearances:")
+    for val in sorted(lett_appears, reverse = True):
+        sorted_appears[lett_appears[val]] = val
+    for key in sorted_appears.keys():
+        lgr.info(f"\t{key}: {sorted_appears[key]}")
+
     if save_option:
-        save_to_json(f"{min_size}-{max_size}_letter-frequencies", sorted_freqs)
+        lgr.info(f"\nsolve and display elapsed time = {time.perf_counter() - start} seconds.")
+        save_dict = {"Letter frequencies":sorted_freqs, "Letter appearances":sorted_appears}
+        saved_file = save_to_json(f"{min_size}-{max_size}_letter-freqs-appears", save_dict)
+        lgr.info(f"\n\t Saved results to '{saved_file}'")
 
-def process_args():
-    arg_parser = ArgumentParser(description="get the save-to-file, minimum and maximum word size options", prog="python3 letter_frequencies.py")
+def set_args():
+    arg_parser = ArgumentParser(description = "Get the save-to-file, file path, minimum and maximum word-size options:",
+                                prog = f"python3 {get_filename(argv[0])}")
     # optional arguments
-    arg_parser.add_argument('-s', '--save', action="store_true", default=False, help="Write the results to a JSON file")
-    arg_parser.add_argument('-n', '--minimum', type=int, default=MIN_WORD_LEN, help="minimum number of letters in each found word")
-    arg_parser.add_argument('-x', '--maximum', type=int, default=MAX_WORD_LEN, help="maximum number of letters in each found word")
+    arg_parser.add_argument('-s', '--save', action = "store_true", default = False,
+                            help = "Save the results to a JSON file.")
+    arg_parser.add_argument('-i', '--input', type = str, metavar = "inPATH", default = DEFAULT_WORD_FILE,
+                            help = f"Path to a file with the list of words to use; DEFAULT = '{DEFAULT_WORD_FILE}'.")
+
+    arg_parser.add_argument('-n', '--minimum', type = int, default = DEFAULT_WORD_LEN,
+                            help = f"MINIMUM number of letters in each word used, "
+                                   f"SMALLEST = {MIN_WORD_LEN}, DEFAULT = {DEFAULT_WORD_LEN}.")
+    arg_parser.add_argument('-x', '--maximum', type = int, default = MIN_WORD_LEN,
+                            help = f"MAXIMUM number of letters in each word used, "
+                                   f"LARGEST = {MAX_WORD_LEN}, DEFAULT = {DEFAULT_WORD_LEN}.")
     return arg_parser
 
-def prep_freqs(argl:list) -> (bool, int, int):
-    args = process_args().parse_args(argl)
+def get_args(argl:list):
+    args_log_level = DEFAULT_LOG_LEVEL
+    args = set_args().parse_args(argl)
 
-    lgr.info("START LOGGING")
-    show(f"save option = '{args.save}'")
+    lgr.log(args_log_level, f"save option = '{args.save}'.")
 
-    lower = args.minimum if MIN_WORD_LEN <= args.minimum <= MAX_WORD_LEN else MIN_WORD_LEN
-    show(f"minimum word size = {lower}")
+    if osp.isfile(args.input):
+        infile = args.input
+        lgr.log(args_log_level, f"input file = '{infile}'.")
+    else:
+        infile = DEFAULT_WORD_FILE
+        lgr.warning(f">> BAD input file = '{args.input}'!! >> Using default file = '{infile}'.")
+
+    lower = args.minimum if MIN_WORD_LEN <= args.minimum <= MAX_WORD_LEN else DEFAULT_WORD_LEN
+    lgr.log(args_log_level, f"minimum word size = {lower}.")
 
     upper = args.maximum if lower <= args.maximum <= MAX_WORD_LEN else lower
-    show(f"maximum word size = {upper}")
+    lgr.log(args_log_level, f"maximum word size = {upper}.\n")
 
-    return args.save, lower, upper
+    return args.save, infile, lower, upper
 
 
-if __name__ == "__main__":
-    log_control = MhsLogger(get_base_filename(__file__))
+log_control = MhsLogger( get_base_filename(__file__), con_level = DEFAULT_LOG_LEVEL )
+
+if __name__ == '__main__':
+    start = time.perf_counter()
     lgr = log_control.get_logger()
-    show = log_control.show
-
-    save_option, min_size, max_size = prep_freqs(argv[1:])
-    run_freqs()
-    show(f"\nfinal elapsed time = {time.perf_counter() - start}")
-    exit()
+    code = 0
+    try:
+        save_option, word_file, min_size, max_size = get_args(argv[1:])
+        lgr.info("START MAIN LOGGING")
+        run()
+    except KeyboardInterrupt as mki:
+        lgr.exception(mki)
+        code = 13
+    except ValueError as mve:
+        lgr.exception(mve)
+        code = 27
+    except Exception as mex:
+        lgr.exception(mex)
+        code = 66
+    lgr.info(f"\n\tTotal elapsed time = {time.perf_counter() - start} seconds.")
+    exit(code)
