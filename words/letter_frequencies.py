85b062d9182fa9036a0cc50daf8cc769d636bbf1
epistemik <epistemik@gmail.com>
2026-02-07 16:24:27 -0500

    fix reverse sorts to accept duplicate numbers; updates to logging; &

--- words/letter_frequencies.py
+++ words/letter_frequencies.py
@@ -2,7 +2,8 @@
 # coding=utf-8
 #
 # letter_frequencies.py
-# -- get the frequency and number of appearances of each letter in words of a specified length from a word list
+# -- get the total number of times each letter is found and the number of words each letter appears in,
+#    in words of a specified length from a specified word list
 #
 # Copyright (c) 2026 Mark Sattolo <epistemik@gmail.com>
 
@@ -10,27 +11,27 @@ __author__         = "Mark Sattolo"
 __author_email__   = "epistemik@gmail.com"
 __python_version__ = "3.6+"
 __created__ = "2023-10-29"
-__updated__ = "2026-01-06"
+__updated__ = "2026-02-06"
 
 import string
 import time
-import json
 from argparse import ArgumentParser
+from collections import defaultdict
 from sys import path, argv
 path.append("/home/marksa/git/Python/utils")
 from mhsUtils import *
 from mhsLogging import *
 
-start = time.perf_counter()
 DEFAULT_WORD_FILE = "input/scrabble-plus.json"
 MIN_WORD_LEN     =  4
 DEFAULT_WORD_LEN =  5
 MAX_WORD_LEN     = 15
 
 def run():
-    """Get the frequency and number of appearances of each letter in words of a specified length from a word list."""
-    freqs = dict.fromkeys(string.ascii_uppercase, 0)
-    appearances = dict.fromkeys(string.ascii_uppercase, 0)
+    """Get the total number of times each letter is found and the number of words each letter appears in,
+       in words of a specified length from a specified word list."""
+    freqs = dict.fromkeys(string.ascii_uppercase, 0) # total number of times each letter is found
+    appearances = dict.fromkeys(string.ascii_uppercase, 0) # number of words each letter appears in
     word_count = 0
     letter_count = 0
     wds = json.load( open(word_file) )
@@ -48,47 +49,50 @@ def run():
     lgr.info(f"\tword count = {word_count}")
     lgr.info(f"\tletter count = {letter_count}\n")
 
-    lett_freqs = {}
+    lett_freqs = defaultdict(str) # dict {freqs -> letter}
     total_freqs = 0
     lgr.info(f"\n\tletter frequencies:")
     for key in freqs.keys():
         lgr.info(f"\t{key}: {freqs[key]}")
         total_freqs += freqs[key]
-        # hopefully there are not two frequencies exactly the same
-        lett_freqs[freqs[key]] = key
+        # concatenate letters in case there are any identical frequencies
+        lett_freqs[freqs[key]] += key
     lgr.info(f"total letter frequencies = {total_freqs}")
 
     sorted_freqs = {}
+    for item in sorted(lett_freqs, reverse = True):
+        sorted_freqs[lett_freqs[item]] = item
     lgr.info(f"\n\tsorted letter frequencies:")
-    for val in sorted(lett_freqs, reverse = True):
-        lgr.info(f"\t{lett_freqs[val]}: {val}")
-        sorted_freqs[lett_freqs[val]] = val
+    for key in sorted_freqs.keys():
+        lgr.info(f"\t{key}: {sorted_freqs[key]}")
 
-    lett_appears = {}
+    lett_appears = defaultdict(str) # dict {appearances -> letter}
     total_appears = 0
     lgr.info(f"\n\tletter appearances:")
     for key in appearances.keys():
         lgr.info(f"\t{key}: {appearances[key]}")
         total_appears += appearances[key]
-        # hopefully there are not two appearance totals exactly the same
-        lett_appears[appearances[key]] = key
+        # concatenate letters in case there are any identical appearance totals
+        lett_appears[appearances[key]] += key
     lgr.info(f"total letter appearances = {total_appears}")
 
     sorted_appears = {}
-    lgr.info(f"\n\tsorted letter appearances:")
     for val in sorted(lett_appears, reverse = True):
         sorted_appears[lett_appears[val]] = val
+    lgr.info(f"\n\tsorted letter appearances:")
     for key in sorted_appears.keys():
         lgr.info(f"\t{key}: {sorted_appears[key]}")
 
     if save_option:
         lgr.info(f"\nsolve and display elapsed time = {time.perf_counter() - start} seconds.")
-        save_dict = {"Letter frequencies":sorted_freqs, "Letter appearances":sorted_appears}
+        save_dict = {"Letter totals":sorted_freqs, "Letter appearances":sorted_appears}
         saved_file = save_to_json(f"{min_size}-{max_size}_letter-freqs-appears", save_dict)
-        lgr.info(f"\n\t Saved results to '{saved_file}'")
+        lgr.info(f"\n\t Saved results to '{saved_file}'.")
 
 def set_args():
-    arg_parser = ArgumentParser(description = "Get the save-to-file, file path, minimum and maximum word-size options:",
+    arg_parser = ArgumentParser(description = "Get the total number of times each letter is found "
+                                              "and the number of words each letter appears in, "
+                                              "in words of a specified length from a specified word list.",
                                 prog = f"python3 {get_filename(argv[0])}")
     # optional arguments
     arg_parser.add_argument('-s', '--save', action = "store_true", default = False,
@@ -118,10 +122,10 @@ def get_args(argl:list):
         lgr.warning(f">> BAD input file = '{args.input}'!! >> Using default file = '{infile}'.")
 
     lower = args.minimum if MIN_WORD_LEN <= args.minimum <= MAX_WORD_LEN else DEFAULT_WORD_LEN
-    lgr.log(args_log_level, f"minimum word size = {lower}.")
+    lgr.log(args_log_level, f"minimum word size = {lower}")
 
     upper = args.maximum if lower <= args.maximum <= MAX_WORD_LEN else lower
-    lgr.log(args_log_level, f"maximum word size = {upper}.\n")
+    lgr.log(args_log_level, f"maximum word size = {upper}\n")
 
     return args.save, infile, lower, upper
 
@@ -134,7 +138,7 @@ if __name__ == '__main__':
     code = 0
     try:
         save_option, word_file, min_size, max_size = get_args(argv[1:])
-        lgr.info("START MAIN LOGGING")
+        lgr.info("START MAIN LOGGING.")
         run()
     except KeyboardInterrupt as mki:
         lgr.exception(mki)
