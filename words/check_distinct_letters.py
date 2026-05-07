5dbde5af48ad909266a643fc7805d327a0deb946
epistemik <epistemik@gmail.com>
2026-05-07 14:19:14 -0400

    enter words in a terminal and find out how many distinct letters each has

---
+++ words/check_distinct_letters.py
@@ -0,0 +1,53 @@
+##############################################################################################################################
+# coding=utf-8
+#
+# check_distinct_letters.py
+#   -- user can continuously enter a word in a terminal and find out how many distinct letters each has
+#
+# Copyright (c) 2026 Mark Sattolo <epistemik@gmail.com>
+
+__author__         = "Mark Sattolo"
+__author_email__   = "epistemik@gmail.com"
+__python_version__ = "3.6+"
+__created__ = "2026-05-07"
+__updated__ = "2026-05-07"
+
+from sys import path, argv
+path.append("/home/marksa/git/Python/utils")
+from mhsUtils import get_filename
+
+def run():
+    """User can continuously enter a word in a terminal and find out how many distinct letters each has."""
+    while True:
+        word = input("Enter a word to check ('EXIT' to quit): ")
+        result = ""
+        if word == "EXIT":
+            print("Bye!\n")
+            break
+            return
+        for it in word:
+            if not it.isalpha():
+                continue
+            lett = it.upper()
+            if lett not in result:
+                result += lett
+        print(f"{len(result)} distinct letters: [{result}]")
+
+
+if __name__ == '__main__':
+    if len(argv) > 1:
+        print(f"Usage: python3 {get_filename(argv[0])}\nEnter words and find out how many distinct letters each has.")
+        exit(0)
+    code = 0
+    try:
+        run()
+    except KeyboardInterrupt as mki:
+        print(mki)
+        code = 13
+    except ValueError as mve:
+        print(mve)
+        code = 27
+    except Exception as mex:
+        print(mex)
+        code = 66
+    exit(code)
